6940a850c98b37931b43645b4457cc0e5e0ad34b
SeyoungCho <wnl383@naver.com>
2023-08-05 02:16:08 +0900

    chore: redux-toolkit, react-redux 패키지 설치 및 리덕스 보일러플레이팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 };
 export default App;
