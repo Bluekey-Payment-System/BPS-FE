ed66c09a0e76758177364733fcc6c4fae59c22f3
SeyoungCho <wnl383@naver.com>
2023-08-05 19:27:57 +0900

    chore: jest config파일 cjs 모듈로 변환

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-import nextJest from 'next/jest';
- 
+const nextJest = require('next/jest');
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
@@ -17,4 +17,4 @@ const config = {
 }
 
  // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+module.exports =  createJestConfig(config)
