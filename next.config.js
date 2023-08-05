cd32dc595ecc5ce6dc95fe7f182550b943779a58
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-05 14:05:25 +0900

    style: 글로벌 변수 전역 사용 기능 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path'); 
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "src/styles/variables.scss"; @import "src/styles/mixins.scss";`, 
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
