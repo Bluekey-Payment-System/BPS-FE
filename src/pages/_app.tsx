dcfa38d47a8d11d3ce8b498a7ffadbbbfde74631
SeyoungCho <wnl383@naver.com>
2023-08-05 17:00:06 +0900

    chore: lint에러 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+
 import { Provider } from "react-redux";
+
 import { store } from "@/redux/store";
 import Pretendard from "@/styles/local.font";
 
@@ -8,7 +10,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <main className={Pretendard.className}>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </main>
     </Provider>
   );
--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,7 @@
-import { useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "@/redux/store";
+
 import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "./store";
+import { useDispatch, useSelector } from "react-redux";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
