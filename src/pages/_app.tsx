38bbeb216809b68a1356cde4f3f7c87c33521596
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-05 16:55:05 +0900

    chore: font-face 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
+import Pretendard from "@/styles/local.font";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />;
+      <main className={Pretendard.className}>
+        <Component {...pageProps} />;
+      </main>
     </Provider>
   );
 };
---
+++ src/styles/local.font.ts
@@ -0,0 +1,18 @@
+import localFont from 'next/font/local'
+
+const Pretendard = localFont({
+    src: [
+      {
+        path: './fonts/Pretendard-Regular.subset.woff',
+        weight: '400',
+        style: 'normal',
+      },
+      {
+        path: './fonts/Pretendard-SemiBold.subset.woff',
+        weight: '600',
+        style: 'normal',
+      },
+    ],
+  })
+
+  export default Pretendard
