b9234dc031ef991506bed5b6a173f26e1b54912e
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-06 16:03:16 +0900

    feat: useOutsideClick 커스텀 훅 추가

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,23 @@
+import { RefObject, useEffect } from "react";
+
+type CallbackFunction = () => void;
+
+const useOutsideClick = (
+  ref: RefObject<HTMLElement>,
+  callback: CallbackFunction,
+) => {
+  const handleClick = (e: MouseEvent) => {
+    if (ref.current && !ref.current.contains(e.target as Node)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick, { capture: true });
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  });
+};
+
+export default useOutsideClick;
