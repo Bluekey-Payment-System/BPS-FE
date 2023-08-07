739371818a4f0e14e13e5bd1846eec4613c00cd0
drizzle96 <dlxkffldk37@naver.com>
2023-08-07 16:47:54 +0900

    feat: 버튼 기본 인터페이스 설정

---
+++ src/components/common/CommonBtns/Button/Button.tsx
@@ -0,0 +1,24 @@
+import classNames from "classnames/bind";
+
+import styles from "./Button.module.scss";
+
+const cx = classNames.bind(styles);
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size: "large" | "medium" | "small";
+  theme: "bright" | "dark";
+  onClick?: () => void;
+  onSubmit?: () => void;
+  isTextEmphasis?: boolean;
+  children: React.ReactNode;
+}
+
+const Button = ({
+  size, theme, onClick, onSubmit, isTextEmphasis = false, children, ...props
+}: ButtonProps) => {
+  return (
+    <button className={cx("button", { [size]: size, [theme]: theme, isTextEmphasis })} onClick={onClick} onSubmit={onSubmit} {...props}>{children}</button>
+  );
+};
+
+export default Button;
