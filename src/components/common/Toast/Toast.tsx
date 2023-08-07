a00978b78cc28181a64f865bab3146d4e16f97e6
SeyoungCho <wnl383@naver.com>
2023-08-07 17:29:44 +0900

    feat: Toast 컴포넌트 추가

---
+++ src/components/common/Toast/Toast.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+import classNames from "classnames/bind";
+import Image from "next/image";
+
+import styles from "./Toast.module.scss";
+
+const cx = classNames.bind(styles);
+
+interface ToastProps {
+  message: string;
+}
+
+/**
+ * 토스트 컴포넌트
+ * @author [SeyoungCho](https://github.com/seyoungcho)
+ * @param {string } message - 토스트 메세지
+ * @example
+ *
+ * ```
+ * <ToastPortal>
+ *   <Toast message="완료되었습니다" />
+ * </ToastPortal>
+ *```
+ */
+const Toast = ({
+  message,
+}: ToastProps) => {
+  const [isShowing, setIsShowing] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsShowing(false);
+    }, 2800);
+
+    return () => { return clearTimeout(timeout); };
+  }, []);
+
+  return (
+    <div
+      className={
+          cx(styles.toastContainer, {
+            [styles.fadeIn]: isShowing,
+            [styles.fadeOut]: !isShowing,
+          })
+        }
+    >
+      <div className={styles.iconBox}>
+        <Image src="/images/lightening.png" alt="번개 아이콘" fill />
+      </div>
+      <span>{message}</span>
+    </div>
+  );
+};
+
+export default Toast;
