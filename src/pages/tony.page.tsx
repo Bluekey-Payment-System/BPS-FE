f6d1ecd478909cdb0ea134223b5505d445ed24ee
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-08 15:57:41 +0900

    chore: 린트 에러 수정

--- src/pages/tony.page.tsx
+++ src/pages/tony.page.tsx
@@ -1,24 +1,18 @@
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useRef } from "react";
 
 import Dropdown from "@/components/common/Dropdown/Dropdown";
 import SearchBar from "@/components/common/SearchBar/SearchBar";
 
 const TonyPage = () => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [dropdownValue, setDropdownValue] = useState("");
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log(dropdownValue, inputRef.current.value);
-  };
-
-  const onSubmitDropdownData = (selectedDropdownValue) => {
-    setDropdownValue(selectedDropdownValue);
   };
 
   return (
     <form style={{ display: "flex" }} onSubmit={handleSubmit}>
-      <Dropdown dropdownListData={["곡 명", "앨범 명"]} onSubmitDropdownData={onSubmitDropdownData} theme="withSearchBar" />
+      <Dropdown dropdownListData={["곡 명", "앨범 명"]} theme="withSearchBar" hasSearchBar />
       <SearchBar placeholder="앨범명을 검색해주세요." ref={inputRef} />
     </form>
   );
