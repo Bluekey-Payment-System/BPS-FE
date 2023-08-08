f7f298bc1b98d074542983c49198ef9ad06b026c
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-08 12:10:14 +0900

    feat: searchBar 기능 추가

---
+++ src/components/common/SearchBar/SearchBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import classNames from "classnames/bind";
+import Image from "next/image";
+
+import styles from "./SearchBar.module.scss";
+
+const cx = classNames.bind(styles);
+interface SearchBarProps {
+  placeholder: string
+}
+
+const SearchBar = (
+  { placeholder }: SearchBarProps,
+  inputRef: React.ForwardedRef<HTMLInputElement>,
+) => {
+  return (
+    <div className={cx("searchBarContainer")}>
+      <input className={cx("searchInput")} ref={inputRef} placeholder={placeholder} />
+      <Image src="/images/search-bar.svg" className={cx("searchImage")} alt="검색" width={13} height={15} />
+    </div>
+  );
+};
+
+export default React.forwardRef(SearchBar);
