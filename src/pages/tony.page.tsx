dad8f8eadecae9b8d31b18c6e45a7d35011ae4a2
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-08 12:32:11 +0900

    chore: lint 에러 수정

--- src/pages/tony.page.tsx
+++ src/pages/tony.page.tsx
@@ -2,7 +2,7 @@ import Dropdown from "@/components/common/Dropdown/Dropdown";
 
 const TonyPage = () => {
   return (
-    <Dropdown dropdownListData={["혁기", "아이유", "볼빨간사춘기", "할아버지"]} withInput />
+    <Dropdown dropdownListData={["혁기", "아이유", "볼빨간사춘기", "할아버지"]} hasSearchBar />
   );
 };
 
