87dfa5bf8b53ebfefcbe078c8e90d0348c94c9de
hayoung-99 <nhy990130@gmail.com>
2023-08-08 14:31:50 +0900

    feat: Tooltip Portal mount 검증

--- src/components/common/Tooltip/TooltipPortal.tsx
+++ src/components/common/Tooltip/TooltipPortal.tsx
@@ -1,22 +1,15 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-// const TooltipPortal = ({ children }: { children: React.ReactNode }) => {
-//   const [mounted, setMounted] = useState(false);
-//   useEffect(() => {
-//     setMounted(true);
-//     return () => { return setMounted(false); };
-//   }, []);
-
-//   const tooltipRoot = typeof window !== "undefined" && document.getElementById("tooltip-root");
-//   if (!mounted) return null;
-//   return createPortal(children, tooltipRoot as HTMLElement);
-// };
-
-// export default TooltipPortal;
-
 const TooltipPortal = ({ children }: { children: React.ReactNode }) => {
-  const tooltipRoot = document.getElementById("tooltip-root");
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+    return () => { return setMounted(false); };
+  }, []);
+
+  const tooltipRoot = typeof window !== "undefined" && document.getElementById("tooltip-root");
+  if (!mounted) return null;
   return createPortal(children, tooltipRoot as Element);
 };
 
