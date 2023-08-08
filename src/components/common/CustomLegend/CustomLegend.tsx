31322b052e199762378a6e1b01a9a2f9e11f9383
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-08 16:34:59 +0900

    chore: 코드리뷰 반영

--- src/components/common/CustomLegend/CustomLegend.tsx
+++ src/components/common/CustomLegend/CustomLegend.tsx
@@ -7,20 +7,20 @@ const cx = classNames.bind(styels);
 interface CustomLegendProps {
   color: string,
   value: string,
-  type: "bar" | "donut"
+  type: "bar" | "doughnut"
 }
 
 /**
  * @author 임병욱
- * @color 컬러 설정
- * @value 아티스트명, 곡명, 매출액, 정산액
- * @type bar차트, donut차트에 따라 마진값을 다르게 주기위해 사용합니다
+ * @param color 컬러 설정
+ * @param value 아티스트명, 곡명, 매출액, 정산액
+ * @param type bar차트, doughnut차트 따라 마진값을 다르게 주기위해 사용합니다
 */
 const CustomLegend = ({ color, value, type }: CustomLegendProps) => {
   return (
     <div className={cx("customLegendContainer")}>
       <div className={cx("color")} style={{ backgroundColor: color }} />
-      <p className={cx("description", type === "bar" ? "bar" : "donut")}>{value}</p>
+      <p className={cx("description", type)}>{value}</p>
     </div>
   );
 };
