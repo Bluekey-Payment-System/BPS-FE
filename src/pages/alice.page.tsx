e8837b5343398ed7707600caac72109632f41d7f
hayoung-99 <nhy990130@gmail.com>
2023-08-09 15:07:04 +0900

    chore: 사용 예시 설명 수정

--- src/pages/alice.page.tsx
+++ src/pages/alice.page.tsx
@@ -1,7 +1,7 @@
 /**
  * EmptyData 컴포넌트 사용 예시
  *
- * - EmptyData를 감싸는 컨테이너 요소에 flex 속성 부여해서 "alignItems: center"로 세로 정렬해야 합니다.
+ * - EmptyData를 감싸는 컨테이너 요소에 flex 속성 부여해서 가운데 정렬해야 합니다.
  */
 import EmptyData from "@/components/common/EmptyData/EmptyData";
 
@@ -10,7 +10,7 @@ const Home = () => {
     <div style={{ backgroundColor: "lightgray", padding: "50px" }}>
 
       <div style={{
-        backgroundColor: "white", height: 800, display: "flex", alignItems: "center",
+        backgroundColor: "white", height: 800, display: "flex", justifyContent: "center", alignItems: "center",
       }}
       >
         <EmptyData type="no-search-result" />
