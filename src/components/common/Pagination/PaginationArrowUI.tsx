6f7c34642ba95fa146ad96018087bb8e957e6f42
drizzle96 <dlxkffldk37@naver.com>
2023-08-09 16:35:09 +0900

    feat: 버튼에 호버됐을 때 색상 변경 구현

--- src/components/common/Pagination/PaginationArrowUI.tsx
+++ src/components/common/Pagination/PaginationArrowUI.tsx
@@ -1,3 +1,9 @@
+import classnames from "classnames/bind";
+
+import styles from "./PaginationArrowUI.module.scss";
+
+const cx = classnames.bind(styles);
+
 interface PaginationArrowUIProps {
   direction?: "prev" | "next",
   able: boolean
@@ -7,7 +13,7 @@ const PaginationArrowUI = ({
   direction = "prev", able,
 }: PaginationArrowUIProps) => {
   return (
-    <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className={cx("arrow")} width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M7 1L1 6.26316L7 11" stroke={able ? "#A3AAB6" : "#EDEEF0"} strokeLinecap="round" strokeLinejoin="round" transform={direction === "next" ? "rotate(180 4 6)" : undefined} />
     </svg>
   );
