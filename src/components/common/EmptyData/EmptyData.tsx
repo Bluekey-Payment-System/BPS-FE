b4e31bbbf8d78013c1b238a1af43f8143e8a514d
hayoung-99 <nhy990130@gmail.com>
2023-08-09 15:33:09 +0900

    refactor: Image 속성 관련 mapper 추가

--- src/components/common/EmptyData/EmptyData.tsx
+++ src/components/common/EmptyData/EmptyData.tsx
@@ -8,16 +8,24 @@ interface EmptyDataProps {
   text?: string
 }
 
+const typeMapper = {
+  "no-data": {
+    src: "/images/empty-data.svg",
+    alt: "데이터 없음",
+  },
+  "no-search-result": {
+    src: "/images/empty-search-result.svg",
+    alt: "검색 결과 없음",
+  },
+};
+
 const cx = classNames.bind(styles);
 
 const EmptyData = ({ type, text }: EmptyDataProps) => {
   return (
     <div className={cx("container")}>
       <div className={cx("imageBox")}>
-        {type === "no-data"
-        && <Image src="/images/empty-data.svg" width={225} height={159} alt="데이터 없음" />}
-        {type === "no-search-result"
-        && <Image src="/images/empty-search-result.svg" width={225} height={159} alt="검색 결과 없음" />}
+        <Image src={typeMapper[type].src} width={225} height={159} alt={typeMapper[type].alt} />
       </div>
       <p className={cx("description")}>
         {text || "검색 결과가 없습니다."}
