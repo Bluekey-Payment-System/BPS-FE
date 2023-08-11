4cd0a4bbe5164052ff43555ccb82a1d4d499f448
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-11 16:07:55 +0900

    chore: lint 수정

--- src/pages/tony.page.tsx
+++ src/pages/tony.page.tsx
@@ -1,48 +1,13 @@
-import dynamic from "next/dynamic";
+import { useState } from "react";
 
-const data = {
-  contents: [
-    {
-      month: 7,
-      settlement: 34,
-      revenue: 50,
-    },
-    {
-      month: 8,
-      settlement: 24,
-      revenue: 65,
-    },
-    {
-      month: 9,
-      settlement: 38,
-      revenue: 12,
-    }, {
-      month: 10,
-      settlement: 89,
-      revenue: 67,
-    }, {
-      month: 11,
-      settlement: 54,
-      revenue: 27,
-    },
-    {
-      month: 12,
-      settlement: 1,
-      revenue: 2,
-    },
-  ],
-};
-
-const barChartData = data.contents;
-
-const DynamicBarChart = dynamic(() => { return import("@/components/common/Chart/BarChart/BarChart"); }, { ssr: false });
+import Dropdown from "@/components/common/Dropdown/Dropdown";
 
 const TonyPage = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [value, setValue] = useState("");
   return (
-    <div style={{ width: "550px", height: "380px" }}>
-      <DynamicBarChart barChartData={barChartData} />
+    <div style={{ display: "flex" }}>
+      <Dropdown dropdownListData={["아이유", "볼빨간 사춘기", "성시경"]} theme="dark" hasSearchBar />
     </div>
   );
 };
