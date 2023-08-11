7a50bda13540824bc1ea515096485351c9894b22
hayoung-99 <nhy990130@gmail.com>
2023-08-11 17:03:40 +0900

    feat: EditButton style 업데이트 & AlbumCard에 연결

--- src/components/common/AlbumCard/EditButton.tsx
+++ src/components/common/AlbumCard/EditButton.tsx
@@ -18,28 +18,28 @@ const EditButton = () => {
   };
 
   return (
-    // <div style={{ position: "relative" }}>
-    //   <button type="button" onClick={handleClickEditButton}>
-    //     <Image src="/images/kebab.svg" width={16} height={16} alt="더보기" />
-    //   </button>
-    //   {isOpen
-    //       && (
-    //       <Popover
-    //         top="10px" // popoverParent기준으로 해당 팝오버는 10px 아래에서 나타남
-    //         onClose={() => { setIsOpen(false); }}
-    //       >
-    //         <div style={{ backgroundColor: "red" }}>팝오버 내용</div>
-    //       </Popover>
-    //       )}
-    // </div>
-    <ul className={cx("buttonList")}>
-      <li className={cx("buttonListItem")}>
-        <button className={cx("button")} type="button">수정하기</button>
-      </li>
-      <li className={cx("buttonListItem")}>
-        <button className={cx("button")} type="button">삭제하기</button>
-      </li>
-    </ul>
+    <div className={cx("kebabButton")}>
+      <button type="button" onClick={handleClickEditButton}>
+        <Image src="/images/kebab.svg" width={16} height={16} alt="더보기" />
+      </button>
+      {isOpen
+          && (
+          <Popover
+            top="27px"
+            right="-4px"
+            onClose={() => { setIsOpen(false); }}
+          >
+            <ul className={cx("buttonList")}>
+              <li className={cx("buttonListItem")}>
+                <button className={cx("button")} type="button">수정하기</button>
+              </li>
+              <li className={cx("buttonListItem")}>
+                <button className={cx("button")} type="button">삭제하기</button>
+              </li>
+            </ul>
+          </Popover>
+          )}
+    </div>
   );
 };
 
