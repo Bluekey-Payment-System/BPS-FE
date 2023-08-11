9d00d536c71e973f72a3c7a3a3f837557b1cfb4a
hayoung-99 <nhy990130@gmail.com>
2023-08-11 16:38:03 +0900

    feat: EditButton UI 제작

--- src/components/common/AlbumCard/EditButton.tsx
+++ src/components/common/AlbumCard/EditButton.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
+import classNames from "classnames/bind";
 import Image from "next/image";
+
+import Popover from "../Popover/Popover";
+
+import styles from "./EditButton.module.scss";
+
+const cx = classNames.bind(styles);
 
 const EditButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -11,12 +18,28 @@ const EditButton = () => {
   };
 
   return (
-    <>
-      <button type="button" onClick={handleClickEditButton}>
-        <Image src="/images/kebab.svg" width={16} height={16} alt="더보기" />
-      </button>
-      {isOpen && <div>클릭!!</div>}
-    </>
+    // <div style={{ position: "relative" }}>
+    //   <button type="button" onClick={handleClickEditButton}>
+    //     <Image src="/images/kebab.svg" width={16} height={16} alt="더보기" />
+    //   </button>
+    //   {isOpen
+    //       && (
+    //       <Popover
+    //         top="10px" // popoverParent기준으로 해당 팝오버는 10px 아래에서 나타남
+    //         onClose={() => { setIsOpen(false); }}
+    //       >
+    //         <div style={{ backgroundColor: "red" }}>팝오버 내용</div>
+    //       </Popover>
+    //       )}
+    // </div>
+    <ul className={cx("buttonList")}>
+      <li className={cx("buttonListItem")}>
+        <button className={cx("button")} type="button">수정하기</button>
+      </li>
+      <li className={cx("buttonListItem")}>
+        <button className={cx("button")} type="button">삭제하기</button>
+      </li>
+    </ul>
   );
 };
 
