f1c323db7ec4a00a501646ce7f01e208169211ec
hayoung-99 <nhy990130@gmail.com>
2023-08-12 02:21:04 +0900

    docs: 예시 코드 alice page에 추가
    테스트용으로 next.config.js에 추가한 s3 url 삭제했습니다.
    현재는 외부 이미지 url을 가져다 사용하지 못하므로 테스트할 시
    albumCoverUrl prop에 null을 기입해주세요.

--- next.config.js
+++ next.config.js
@@ -9,17 +9,6 @@ const nextConfig = {
     prependData: `@use "src/styles/variables.scss" as *; @use "src/styles/mixins.scss" as *;`,
   },
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-  // 테스트용 (추후 삭제 예정)
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "bootcamp-project-api.s3.ap-northeast-2.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
