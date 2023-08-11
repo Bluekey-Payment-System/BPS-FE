3e957e3eda7042608075cf62e029ee6dc663565f
drizzle96 <dlxkffldk37@naver.com>
2023-08-11 16:04:10 +0900

    feat: 트랙별 현황 데이터 타입 정의

---
+++ src/types/dto.ts
@@ -0,0 +1,21 @@
+export interface ITrackStatus {
+  track: {
+    id: number
+    name: string
+    enName: string
+  }
+  album: {
+    id: number
+    name: string
+    enName: string
+  }
+  artist: {
+    id: number
+    name: string
+    enName: string
+  }
+  revenue: number | null
+  netIncome: number | null
+  settlementAmount: number | null
+  commissionRate: number // 사측 요율
+}
