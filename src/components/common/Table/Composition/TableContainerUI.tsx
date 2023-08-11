0a327edc7c5c3a047234baf51e595830d794e446
drizzle96 <dlxkffldk37@naver.com>
2023-08-12 01:00:31 +0900

    feat: scroll이 움직인 조건에 shadow가 적용되도록 구현

--- src/components/common/Table/Composition/TableContainerUI.tsx
+++ src/components/common/Table/Composition/TableContainerUI.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from "react";
+
 import classNames from "classnames/bind";
 
 import styles from "./TableContainerUI.module.scss";
@@ -15,10 +17,29 @@ const TableContainerUI = ({
   stickyColumns = [false, false, false],
   children,
 }: TableContainerUIProps) => {
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+  const [isShownColumnShadow, setIsShowColumnShadow] = useState([false, false, false]);
+  const handleScrollTableContainer = () => {
+    // TODO: 스로틀링
+    if (!tableContainerRef.current) return;
+    const { scrollLeft, scrollWidth, clientWidth } = tableContainerRef.current;
+    setIsShowColumnShadow((prev) => {
+      return [(scrollLeft > 0), prev[1], (scrollLeft + 1 < scrollWidth - clientWidth)];
+    });
+  };
+
   return (
     <div className={cx("wrapper")}>
-      <div className={cx("tableContainer")}>
-        <table className={cx("table", { firstSticky: stickyColumns[0], lastSticky: stickyColumns[2] })}>{children}</table>
+      <div className={cx("tableContainer")} onScroll={handleScrollTableContainer} ref={tableContainerRef}>
+        <table className={cx("table", {
+          firstSticky: stickyColumns[0],
+          firstShadow: isShownColumnShadow[0],
+          lastSticky: stickyColumns[2],
+          lastShadow: isShownColumnShadow[2],
+        })}
+        >
+          {children}
+        </table>
       </div>
       {paginationElement && <div className={cx("paginationContainer")}>{paginationElement}</div>}
     </div>
