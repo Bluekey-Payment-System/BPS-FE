7388d68208fe07302b5d71b8665eac3abe7ba4e6
hayoung-99 <nhy990130@gmail.com>
2023-08-13 06:14:44 +0900

    feat: ExcelFileUploader UI 제작
    현재 isDragging state와 관련된 조건부 렌더링이 ux가 좋지 않음

--- src/components/common/ExcelFileUploader/ExcelFileUploader.tsx
+++ src/components/common/ExcelFileUploader/ExcelFileUploader.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
 
+import classNames from "classnames/bind";
+import Image from "next/image";
+
+import useToast from "@/hooks/useToast";
+
 import styles from "./ExcelFileUploader.module.scss"; // 스타일 파일
+
+const cx = classNames.bind(styles);
 
 const ExcelFileUploader = () => {
   const [isDragging, setIsDragging] = useState(false);
+  const { showToast } = useToast();
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -18,8 +26,8 @@ const ExcelFileUploader = () => {
   };
 
   const handleUploadFile = (droppedFiles: File[]) => {
-    // 여기서 드롭된 파일을 처리하면 됩니다.
-    console.log("Dropped files:", droppedFiles);
+    // 여기서 드롭된 파일을 처리
+    showToast(`Dropped file: ${droppedFiles[0].name}`);
   };
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
@@ -33,30 +41,34 @@ const ExcelFileUploader = () => {
 
   return (
     <div
-      style={{
-        width: 500, height: 500, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", backgroundColor: "aliceblue",
-      }}
-      className={`${styles.fileDrop} ${isDragging ? styles.dragging : ""}`}
+      className={cx("uploadContainer")}
       onDragOver={handleDragOver}
       onDragLeave={handleDragLeave}
       onDrop={handleDrop}
     >
       <label
-        className={isDragging ? "DragDrop-File-Dragging" : "DragDrop-File"}
+        className={cx("clickableArea")}
         htmlFor="fileUpload"
-        style={{ cursor: "pointer" }}
       >
-        File Drag and Drop
+        <Image src="/images/upload.svg" width={33} height={33} alt="액셀 파일 업로드" />
+        <h3 className={cx("dragDrop")}>
+          {isDragging ? "Drag & Drop" : "Drop file here"}
+        </h3>
+        <p className={cx("message")}>
+          이곳에 파일을 끌어다 놓으면 파일이 업로드됩니다.
+          <br />
+          파일 유형: xslx, xls
+        </p>
         <input
           type="file"
           id="fileUpload"
           style={{ display: "none" }}
-          onChange={(e) => { return console.log(e); }}
+          onChange={(e) => {
+            showToast(`Dropped file: ${e.target.files && e.target.files.length > 0 && e.target.files[0].name}`);
+          }}
+          accept=".xlsx, .xls"
         />
       </label>
-      <p>
-        {isDragging ? "Drop here" : "Drag and drop files here"}
-      </p>
     </div>
   );
 };
