a0e8beef05ea01ddca281fe02bbc3b47f124a8d1
drizzle96 <dlxkffldk37@naver.com>
2023-08-13 01:24:17 +0900

    docs: MainLayout javadoc 작성

--- src/components/common/Layouts/MainLayout.tsx
+++ src/components/common/Layouts/MainLayout.tsx
@@ -9,6 +9,15 @@ interface MainLayoutProps {
   children: React.ReactNode
 }
 
+/**
+ * #### 페이지 전체를 감싸는 제목과 내용으로만 이루어진 레이아웃
+ * ##### 사용처: 아티스트 등록, 앨범 등록, 앨범 탐색, 아티스트 계정 관리, 내 프로필
+ *
+ * @author 연우킴(drizzle96) [Github](https://github.com/drizzle96)
+ * @param {string} title - 레이아웃의 제목입니다.
+ * @param {React.ReactNode} children - 레이아웃의 내용 컴포넌트입니다.
+ *
+ */
 const MainLayout = ({ title, children }: MainLayoutProps) => {
   return (
     <section className={cx("container")}>
--- src/components/common/Layouts/MainLayoutWithDropdown.tsx
+++ src/components/common/Layouts/MainLayoutWithDropdown.tsx
@@ -10,6 +10,15 @@ interface MainLayoutWithDropdownProps {
   children: React.ReactNode
 }
 
+/**
+ * #### 페이지 전체를 감싸는 제목과 내용, 그리고 제목 옆 드롭다운 요소가 포함된 레이아웃
+ * ##### 사용처: 대시보드, 정산내역 업로드
+ *
+ * @author 연우킴(drizzle96) [Github](https://github.com/drizzle96)
+ * @param {React.ReactNode} dropdownElement - 제목 옆 드롭다운이 포함된 컴포넌트입니다.
+ * @param {React.ReactNode} children - 레이아웃의 내용 컴포넌트입니다.
+ *
+ */
 const MainLayoutWithDropdown = ({
   title,
   dropdownElement,
