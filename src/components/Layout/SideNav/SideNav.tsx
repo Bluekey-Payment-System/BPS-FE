74dc32432d43525f408cb89824b89c45673860dc
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-12 14:40:12 +0900

    feat: 데스크톱 레이아웃 제작

--- src/components/Layout/SideNav/SideNav.tsx
+++ src/components/Layout/SideNav/SideNav.tsx
@@ -1,6 +1,18 @@
+import classNames from "classnames/bind";
+
+import styles from "./SideNav.module.scss";
+import { SUPER_ADMIN } from "./SideNav.utils";
+
+const cx = classNames.bind(styles);
+
 const SideNav = () => {
   return (
-    <div />
+    <aside className={cx("asideContainer")}>
+      {SUPER_ADMIN.map((list) => {
+        return <div className={cx("asideItem")} key={list}>{list}</div>;
+      })}
+
+    </aside>
   );
 };
 
