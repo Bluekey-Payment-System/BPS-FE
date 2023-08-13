fdb50f8953de527a855774c32fdcb99421c9ed45
hayoung-99 <nhy990130@gmail.com>
2023-08-13 23:20:44 +0900

    feat: 로고 클릭 시 대시보드 페이지 이동

--- src/components/Layout/GNB/GNB.tsx
+++ src/components/Layout/GNB/GNB.tsx
@@ -1,6 +1,18 @@
+import classNames from "classnames/bind";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./GNB.module.scss";
+
+const cx = classNames.bind(styles);
+
 const GNB = () => {
   return (
-    <div />
+    <div className={cx("gnbBox")}>
+      <Link href="/dashboard">
+        <Image src="/images/bluekey-logo-pc.svg" width={153} height={36} alt="블루키 뮤직" />
+      </Link>
+    </div>
   );
 };
 
