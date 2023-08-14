094ef25e4acb34ad4422036155d76c3b6fa217d9
hayoung-99 <nhy990130@gmail.com>
2023-08-14 15:01:20 +0900

    feat: 프로필 미등록시 랜덤 프로필 이미지 설정

--- src/components/Layout/GNB/GNB.tsx
+++ src/components/Layout/GNB/GNB.tsx
@@ -3,18 +3,20 @@ import classNames from "classnames/bind";
 import Image from "next/image";
 import Link from "next/link";
 
+import { RANDOM_PROFILES } from "@/constants/randomProfileList";
 import useToast from "@/hooks/useToast";
 
 import styles from "./GNB.module.scss";
+import getRandomProfileIndex from "./GNB.utils";
 
 interface GNBProps {
-  userName: string,
+  loginId: string,
   profileImage: string | null,
 }
 
 const cx = classNames.bind(styles);
 
-const GNB = ({ userName, profileImage }: GNBProps) => {
+const GNB = ({ loginId, profileImage }: GNBProps) => {
   const { showToast } = useToast();
 
   const handleClickNotification = () => {
@@ -37,8 +39,8 @@ const GNB = ({ userName, profileImage }: GNBProps) => {
         <Link href="/my-profile" className={cx("profile")}>
           {profileImage
             ? <Image src={profileImage} width={30} height={30} alt="프로필 이미지" />
-            : <Avatar size={30} name="Alicia Dickerson" variant="marble" colors={["#bfd4f9", "#76a3f2", "#387ffd", "#ffd8d8", "#9b88ed"]} />}
-          <span className={cx("profileName")}>{`${userName} 님`}</span>
+            : <Avatar size={30} name={RANDOM_PROFILES[getRandomProfileIndex(loginId)]} variant="marble" colors={["#bfd4f9", "#76a3f2", "#387ffd", "#ffd8d8", "#9b88ed"]} />}
+          <span className={cx("profileName")}>{`${loginId} 님`}</span>
         </Link>
         <button className={cx("logoutButton")} type="button" onClick={handleLogout}>
           <Image src="/images/logout.svg" width={18} height={15} alt="로그아웃" />
