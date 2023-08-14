17bcfebe49f73e4788c217e93f47a7fe62e23613
drizzle96 <dlxkffldk37@naver.com>
2023-08-14 15:17:48 +0900

    feat: stickyHeader 토대로 미등록 데이터 테이블 제작

---
+++ src/components/common/Table/UnRegisteredDataTable.tsx
@@ -0,0 +1,39 @@
+import { IUnRegisteredData } from "@/types/dto.temp";
+
+import TableBodyUI from "./Composition/TableBodyUI";
+import TableCellUI from "./Composition/TableCellUI";
+import TableContainerUI from "./Composition/TableContainerUI";
+import TableHeaderUI from "./Composition/TableHeaderUI";
+import TableRowUI from "./Composition/TableRowUI";
+
+interface UnRegisteredDataTableProps {
+  data: IUnRegisteredData[]
+}
+
+const UnRegisteredDataTable = ({ data }: UnRegisteredDataTableProps) => {
+  return (
+    <TableContainerUI
+      stickyHeader
+      tableHeight={278}
+    >
+      <TableHeaderUI>
+        <TableCellUI isHeader>행</TableCellUI>
+        <TableCellUI isHeader>분류</TableCellUI>
+        <TableCellUI isHeader>값</TableCellUI>
+      </TableHeaderUI>
+      <TableBodyUI>
+        {data.map((item) => {
+          return (
+            <TableRowUI key={item.id}>
+              <TableCellUI>{item.row}</TableCellUI>
+              <TableCellUI>{item.category}</TableCellUI>
+              <TableCellUI>{item.value}</TableCellUI>
+            </TableRowUI>
+          );
+        })}
+      </TableBodyUI>
+    </TableContainerUI>
+  );
+};
+
+export default UnRegisteredDataTable;
--- src/types/dto.temp.ts
+++ src/types/dto.temp.ts
@@ -19,3 +19,10 @@ export interface ITrackStatus {
   settlementAmount: number | null
   commissionRate: number // 사측 요율
 }
+
+export interface IUnRegisteredData {
+  id: number
+  row: number
+  category: string
+  value: string
+}
