218371213ad1394e08b90046e9b0e65a26e9d030
hayoung-99 <nhy990130@gmail.com>
2023-08-14 14:55:51 +0900

    feat: profileImage가 null일 시 랜덤 프로필 이미지 설정

--- src/components/Layout/GNB/GNB.tsx
+++ src/components/Layout/GNB/GNB.tsx
@@ -1,3 +1,4 @@
+import Avatar from "boring-avatars";
 import classNames from "classnames/bind";
 import Image from "next/image";
 import Link from "next/link";
@@ -16,6 +17,10 @@ const cx = classNames.bind(styles);
 const GNB = ({ userName, profileImage }: GNBProps) => {
   const { showToast } = useToast();
 
+  const handleClickNotification = () => {
+    showToast("알림창 오픈");
+  };
+
   const handleLogout = () => {
     showToast("로그아웃 되었습니다.");
   };
@@ -26,12 +31,14 @@ const GNB = ({ userName, profileImage }: GNBProps) => {
         <Image src="/images/bluekey-music-logo.svg" width={153} height={36} alt="블루키 뮤직" />
       </Link>
       <div className={cx("rightSide")}>
-        <button type="button">
+        <button type="button" onClick={handleClickNotification}>
           <Image src="/images/bell.svg" width={20} height={20} alt="알림" />
         </button>
         <Link href="/my-profile" className={cx("profile")}>
-          <Image className={cx("profileImage")} src="/images/default-profile-image.svg" width={30} height={30} alt="프로필 이미지" />
-          <span className={cx("profileName")}>bluekeymusic_domain 님</span>
+          {profileImage
+            ? <Image src={profileImage} width={30} height={30} alt="프로필 이미지" />
+            : <Avatar size={30} name="Alicia Dickerson" variant="marble" colors={["#bfd4f9", "#76a3f2", "#387ffd", "#ffd8d8", "#9b88ed"]} />}
+          <span className={cx("profileName")}>{`${userName} 님`}</span>
         </Link>
         <button className={cx("logoutButton")} type="button" onClick={handleLogout}>
           <Image src="/images/logout.svg" width={18} height={15} alt="로그아웃" />
