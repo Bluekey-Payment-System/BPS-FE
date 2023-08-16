57667bcecfc4302892a092fc9ad2b758e021b163
SeyoungCho <wnl383@naver.com>
2023-08-16 19:53:31 +0900

    doc: PickerItem jsDoc 추가

--- src/components/common/MonthPicker/PickerItem.tsx
+++ src/components/common/MonthPicker/PickerItem.tsx
@@ -10,7 +10,12 @@ interface PickerItemProps {
   selected: boolean;
   onSelect: (arg1: YearString | MonthString) => void;
 }
-
+/**
+ * @author [SeyoungCho](https://github.com/seyoungcho)
+ * @param textContent {YearStirng | MonthString} 연, 월 선택 버튼 내용(연, 월)
+ * @param selected {boolean} 아이템 선택 여부
+ * @param onSelect {Funtion} 아이템 선택 시 호출할 콜백함수
+ */
 const PickerItem = ({ textContent, selected = false, onSelect }: PickerItemProps) => {
   const handleSelectOption = () => {
     onSelect(textContent);
