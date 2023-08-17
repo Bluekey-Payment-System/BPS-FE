796ec4212c7f43726878e5ed5b90f23f6ef0425d
hayoung-99 <nhy990130@gmail.com>
2023-08-17 22:17:12 +0900

    feat: 정산 내역 업로드 페이지 UI 제작

--- src/pages/admin/upload-revenue/[[...monthYear]].page.tsx
+++ src/pages/admin/upload-revenue/[[...monthYear]].page.tsx
@@ -1,14 +1,19 @@
 import classNames from "classnames/bind";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
+import EmptyData from "@/components/common/EmptyData/EmptyData";
+import ExcelFileUploader from "@/components/common/ExcelFileUploader/ExcelFileUploader";
+import ArtboardLayout from "@/components/common/Layouts/ArtboardLayout";
 import MainLayoutWithDropdown from "@/components/common/Layouts/MainLayoutWithDropdown";
+import SectionHr from "@/components/common/Layouts/SectionHr";
+import SectionLayout from "@/components/common/Layouts/SectionLayout";
 
 import styles from "./[[...monthYear]].module.scss";
 
 const cx = classNames.bind(styles);
 
 const UploadRevenuePage = () => {
-  const router = useRouter();
+  // const router = useRouter();
 
   return (
     <MainLayoutWithDropdown
@@ -19,11 +24,17 @@ const UploadRevenuePage = () => {
         </div>
     )}
     >
-      <h1>
-        monthYear:
-        {" "}
-        {router.query.monthYear}
-      </h1>
+      <ArtboardLayout>
+        <div style={{ width: 730 }}>
+          <SectionLayout title="정산 내역 파일 업로드">
+            <ExcelFileUploader />
+          </SectionLayout>
+          <SectionHr isThick />
+          <SectionLayout title="업로드 내역">
+            <EmptyData type="no-data" text="업로드 내역이 없습니다." />
+          </SectionLayout>
+        </div>
+      </ArtboardLayout>
     </MainLayoutWithDropdown>
   );
 };
