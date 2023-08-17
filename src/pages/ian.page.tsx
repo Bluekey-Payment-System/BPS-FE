d81dfe73d3ea774a6f1168efd092f89ad16221c9
drizzle96 <dlxkffldk37@naver.com>
2023-08-17 14:50:40 +0900

    fix: SideNav position: sticky로 변경

--- src/pages/ian.page.tsx
+++ src/pages/ian.page.tsx
@@ -1,17 +1,14 @@
+import MainLayout from "@/components/common/Layouts/MainLayout";
 import Pagination from "@/components/common/Pagination/Pagination";
 import AdminAccountsTable from "@/components/common/Table/Assemble/AdminAccountsTable";
 import TrackStatusTable from "@/components/common/Table/Assemble/TrackStatusTable";
-import UnRegisteredDataTable from "@/components/common/Table/Assemble/UnRegisteredDataTable";
 import { MOCK_ADMIN_TABLE } from "@/constants/mock";
-import { MOCK_ADMIN_ACCOUNT_DATA, MOCK_UNREGISTERED_DATA } from "@/constants/table";
+import { MOCK_ADMIN_ACCOUNT_DATA } from "@/constants/table";
 
 const Ian = () => {
   return (
-    <>
-      <div style={{
-        maxWidth: "1200px", display: "flex", flexDirection: "column", gap: "50px", marginBottom: "30px",
-      }}
-      >
+    <MainLayout title="페이지 레이아웃">
+      <div style={{ width: "100%" }}>
         <TrackStatusTable
           data={MOCK_ADMIN_TABLE}
           paginationElement={<Pagination activePage={15} totalItems={120} itemsPerPage={6} />}
@@ -21,13 +18,7 @@ const Ian = () => {
           paginationElement={<Pagination activePage={10} totalItems={80} itemsPerPage={4} queryParamName="admin-account-page" />}
         />
       </div>
-      <div style={{
-        maxWidth: "455px", maxHeight: "278px", display: "flex", flexDirection: "column",
-      }}
-      >
-        <UnRegisteredDataTable data={MOCK_UNREGISTERED_DATA} />
-      </div>
-    </>
+    </MainLayout>
   );
 };
 
