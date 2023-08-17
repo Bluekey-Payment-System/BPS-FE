b79063c2753958c1046fa7756174bd77fbfb0604
SeyoungCho <wnl383@naver.com>
2023-08-17 20:04:46 +0900

    test: 어드민, 아티스트 로그인 페이지 테스트코드 추가

---
+++ src/pages/signin/__tests__/index.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from "@testing-library/dom";
+import { useRouter } from "next/router";
+
+import ArtistSigninPage from "@/pages/signin/index.page";
+import { renderWithProviders } from "@/utils/test.utils";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter: jest.fn(),
+  };
+});
+
+describe("아티스트 로그인 페이지 렌더링 테스트", () => {
+  it("에러 없이 렌더링 되어야 합니다.", () => {
+    const push = jest.fn();
+    (useRouter as jest.Mock).mockImplementation(() => {
+      return {
+        push,
+        pathname: "/",
+        route: "/",
+        asPath: "/",
+        query: "",
+      };
+    });
+    renderWithProviders(<ArtistSigninPage />);
+
+    expect(screen.getByText("로그인")).toBeInTheDocument();
+  });
+});
