ea98d05156841f950f24dc2d9e04a10f7a4381c7
drizzle96 <dlxkffldk37@naver.com>
2023-08-17 15:37:38 +0900

    feat: 섹션 구분선 컴포넌트 제작

--- src/pages/ian.page.tsx
+++ src/pages/ian.page.tsx
@@ -1,23 +1,22 @@
+import ArtboardLayout from "@/components/common/Layouts/ArtboardLayout";
 import MainLayout from "@/components/common/Layouts/MainLayout";
-import Pagination from "@/components/common/Pagination/Pagination";
-import AdminAccountsTable from "@/components/common/Table/Assemble/AdminAccountsTable";
-import TrackStatusTable from "@/components/common/Table/Assemble/TrackStatusTable";
-import { MOCK_ADMIN_TABLE } from "@/constants/mock";
-import { MOCK_ADMIN_ACCOUNT_DATA } from "@/constants/table";
+import SectionHr from "@/components/common/Layouts/SectionHr";
+import SectionLayout from "@/components/common/Layouts/SectionLayout";
 
 const Ian = () => {
   return (
     <MainLayout title="페이지 레이아웃">
-      <div style={{ width: "100%" }}>
-        <TrackStatusTable
-          data={MOCK_ADMIN_TABLE}
-          paginationElement={<Pagination activePage={15} totalItems={120} itemsPerPage={6} />}
-        />
-        <AdminAccountsTable
-          data={MOCK_ADMIN_ACCOUNT_DATA}
-          paginationElement={<Pagination activePage={10} totalItems={80} itemsPerPage={4} queryParamName="admin-account-page" />}
-        />
-      </div>
+      <ArtboardLayout>
+        <div style={{ width: "730px" }}>
+          <SectionLayout title="위 섹션">
+            <div>위 섹션 내용물</div>
+          </SectionLayout>
+          <SectionHr isThick />
+          <SectionLayout title="아래 섹션">
+            <div>아래 섹션 내용물</div>
+          </SectionLayout>
+        </div>
+      </ArtboardLayout>
     </MainLayout>
   );
 };
