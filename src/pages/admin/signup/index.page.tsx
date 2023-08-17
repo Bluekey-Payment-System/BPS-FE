4d2ccaa7919f29a4a2d943f7b88a9e177fa9ad23
SeyoungCho <wnl383@naver.com>
2023-08-18 03:48:39 +0900

    feat: 어드민 회원가입 페이지 추가

---
+++ src/pages/admin/signup/index.page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "@/components/auth/SignupForm.tsx/SignupForm";
+
+const AdminSignupPage = () => {
+  return (
+    <SignupForm title="관리자 회원가입" />
+  );
+};
+
+export default AdminSignupPage;
