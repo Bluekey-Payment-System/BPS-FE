9d99fab349c46de6b257a614f77dd422aae1e810
SeyoungCho <wnl383@naver.com>
2023-08-17 17:36:27 +0900

    feat: 아티스트 로그인 페이지 추가

--- src/pages/signin/index.page.tsx
+++ src/pages/signin/index.page.tsx
@@ -1,18 +1,18 @@
 import SigninForm from "@/components/auth/SigninForm";
 import useToast from "@/hooks/useToast";
 
-const AdminSigninPage = () => {
+const ArtistSigninPage = () => {
   const { showToast } = useToast();
-  // TODO: /api/v1/auth/admin/login 에 POST요청
+  // TODO: /api/v1/auth/member/login 에 POST 요청
   // TODO: useFormContext 사용하여 handleSignin함수와 폼 입력값 연동
   const handleSignin = () => {
     return new Promise<void>(() => {
-      setTimeout(() => { showToast("어드민으로 로그인 되었습니다"); }, 1000);
+      setTimeout(() => { showToast("아티스트로 로그인 되었습니다"); }, 1000);
     });
   };
   return (
-    <SigninForm title="관리자 로그인" onSubmit={handleSignin} />
+    <SigninForm title="아티스트 로그인" onSubmit={handleSignin} />
   );
 };
 
-export default AdminSigninPage;
+export default ArtistSigninPage;
