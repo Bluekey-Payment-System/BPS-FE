a6d6d83a914eaf28351e605cd1a6b67e7ec8d78d
SeyoungCho <wnl383@naver.com>
2023-08-17 22:34:19 +0900

    fix: margin대신 Spacing 컴포넌트 사용으로 교체

--- src/components/auth/SigninForm/SigninForm.tsx
+++ src/components/auth/SigninForm/SigninForm.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 import Button from "@/components/common/CommonBtns/Button/Button";
 import PasswordField from "@/components/common/Inputs/PasswordInput/PasswordField";
 import TextField from "@/components/common/Inputs/TextField/TextField";
+import Spacing from "@/components/common/Layouts/Spacing";
 
 import styles from "./SigninForm.module.scss";
 
@@ -25,21 +26,20 @@ const SigninForm = ({ title, onSubmit }:SigninFormProps) => {
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     <form className={cx("form")} onSubmit={handleSubmit(onSubmit)}>
       <h1 className={cx("title")}>{title}</h1>
-      <div className={cx("textFieldWrapper")}>
-        <TextField {...register("loginId")} errors={errors} placeholder="아이디 입력" />
-      </div>
-      <div className={cx("textFieldWrapper")}>
-        <PasswordField {...register("password")} errors={errors} placeholder="비밀번호 입력" />
-      </div>
-      <div className={cx("signinBtnWrapper")}>
-        <Button size="large" theme="dark" type="submit">로그인</Button>
-      </div>
+      <TextField {...register("loginId")} errors={errors} placeholder="아이디 입력" />
+      <Spacing size={16} />
+      <PasswordField {...register("password")} errors={errors} placeholder="비밀번호 입력" />
+      <Spacing size={36} />
+      <Button size="large" theme="dark" type="submit">로그인</Button>
       {isAdmin && (
-        <p>
-          아직 회원이 아니세요?
-          {" "}
-          <Link href="/admin/signup">회원가입</Link>
-        </p>
+        <>
+          <Spacing size={24} />
+          <p>
+            아직 회원이 아니세요?
+            {" "}
+            <Link href="/admin/signup">회원가입</Link>
+          </p>
+        </>
       )}
     </form>
   );
