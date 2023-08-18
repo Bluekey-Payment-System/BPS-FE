329c40b120a39a9240029043f835ee4d1b467824
SeyoungCho <wnl383@naver.com>
2023-08-18 10:13:16 +0900

    doc: getLastSegmentFromUrl 유틸 함수 jsDoc 추가

--- src/utils/getLastSegmentFromUrl.ts
+++ src/utils/getLastSegmentFromUrl.ts
@@ -1,3 +1,15 @@
+/**
+ * getLastSegmentFromUrl 유틸 함수
+ * @author [SeyoungCho](https://github.com/seyoungcho)
+ * @param url {string} "xxx/xxx/xxx/..." 형태의 url 스트링
+ * @returns {string} url을 "/"으로 구분지어 마지막 세그먼트를 스트링으로 반황합니다.
+ * @example
+ * ```ts
+ * const pathname = useRouter.asPath; // "/admin/dashborad/202307"
+ * const monthYearStr = getLastSegmentFromUrl(pathname);
+ * console.log(monthYearStr); // "202307"
+ * ```
+ *  */
 const getLastSegmentFromUrl = (url: string) => {
   const segments = url.split("/");
   return segments[segments.length - 1];
