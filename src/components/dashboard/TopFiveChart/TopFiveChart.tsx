077a1344f91f7040cc4444a5d71591f35b395445
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-19 03:27:29 +0900

    feat: TopFiveChartLegend 컴포넌트 제작

--- src/components/dashboard/TopFiveChart/TopFiveChart.tsx
+++ src/components/dashboard/TopFiveChart/TopFiveChart.tsx
@@ -1,27 +1,25 @@
+/* eslint-disable react/no-array-index-key */
 import classNames from "classnames/bind";
+import { useRouter } from "next/router";
 
-import { chartColor } from "@/components/common/Chart/chart.utils";
 import DoughnutChart from "@/components/common/Chart/DoughnutChart/DoughnutChart";
-import Chip from "@/components/common/Chip/Chip";
-import CustomLegend from "@/components/common/CustomLegend/CustomLegend";
-import { IGetAdminEarningsTopArtistResponse } from "@/services/api/types/admin";
-import { MemberType } from "@/types/enums/user.enum";
-import formatMoney from "@/utils/formatMoney";
+import { IGetAdminEarningsTopArtistResponse, IGetAdminEarningsTopTrackResponse } from "@/services/api/types/admin";
 
 import styles from "./TopFiveChart.module.scss";
+import RouteBasedOnPath from "./TopFiveChart.utils";
+import TopFiveChartLegend from "./TopFiveChartLegend";
 
 const cx = classNames.bind(styles);
 
-interface TopFiveChartProps {
-  topFiveChartData: IGetAdminEarningsTopArtistResponse,
-  type: MemberType
-}
+type TopFiveChartProps = IGetAdminEarningsTopArtistResponse | IGetAdminEarningsTopTrackResponse;
 
-const TopFiveChart = ({ topFiveChartData, type }: TopFiveChartProps) => {
+const TopFiveChart = ({ topFiveChartData }: { topFiveChartData: TopFiveChartProps }) => {
+  const router = useRouter();
+
   return (
     <div className={cx("topFiveChartContainer")}>
       <div className={cx("description")}>
-        <span>당월 Top 5 아티스트 매출 비중</span>
+        <span>{RouteBasedOnPath(router.asPath)}</span>
       </div>
       <div className={cx("contentContainer")}>
         <div style={{ width: "300px", height: "300px" }}>
@@ -30,16 +28,7 @@ const TopFiveChart = ({ topFiveChartData, type }: TopFiveChartProps) => {
         <div className={cx("legendContainer")}>
           {topFiveChartData?.contents.map(((legendData, index) => {
             return (
-              <>
-                <CustomLegend
-                  key={chartColor[index]}
-                  color={chartColor[index]}
-                  value={legendData.artist.koName}
-                  type="doughnut"
-                />
-                <p className={cx("fomattedMoney")}>{formatMoney(legendData.revenue, "chart")}</p>
-                <Chip percentage={legendData.growthRate} />
-              </>
+              <TopFiveChartLegend key={index} legendData={legendData} index={index} />
             );
           }))}
         </div>
---
+++ src/components/dashboard/TopFiveChart/TopFiveChartLegend.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-nested-ternary */
+import classNames from "classnames/bind";
+
+import { chartColor } from "@/components/common/Chart/chart.utils";
+import Chip from "@/components/common/Chip/Chip";
+import CustomLegend from "@/components/common/CustomLegend/CustomLegend";
+import { IDoughnutArtistRevenue, IDoughnutTrackRevenue } from "@/types/dto";
+import formatMoney from "@/utils/formatMoney";
+
+import styles from "./TopFiveChart.module.scss";
+
+const cx = classNames.bind(styles);
+
+interface TopFiveChartLegendProps {
+  legendData: IDoughnutArtistRevenue | IDoughnutTrackRevenue
+  index: number
+}
+
+const TopFiveChartLegend = ({ legendData, index }: TopFiveChartLegendProps) => {
+  return (
+    <div key={index}>
+      {"artist" in legendData ? (
+        <>
+          <CustomLegend
+            key={chartColor[index]}
+            color={chartColor[index]}
+            value={legendData.artist.koArtistName}
+            type="doughnut"
+          />
+          <p className={cx("formattedMoney")}>{formatMoney(legendData.revenue, "chart")}</p>
+          <Chip percentage={legendData.growthRate} />
+        </>
+      ) : "track" in legendData ? (
+        <>
+          <CustomLegend
+            key={chartColor[index]}
+            color={chartColor[index]}
+            value={legendData.track.koTrackName}
+            type="doughnut"
+          />
+          <p className={cx("formattedMoney")}>{formatMoney(legendData.revenue, "chart")}</p>
+          <Chip percentage={legendData.growthRate} />
+        </>
+      ) : null}
+    </div>
+  );
+};
+
+export default TopFiveChartLegend;
