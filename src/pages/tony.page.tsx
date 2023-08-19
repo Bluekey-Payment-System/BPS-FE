e0d00e3902ebae3e6103b60a7aa082a014ca1312
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-19 20:50:57 +0900

    chore: 꺾은선 카드 컴포넌트 이전

--- src/pages/tony.page.tsx
+++ src/pages/tony.page.tsx
@@ -1,72 +1,148 @@
-import TopFiveChart from "@/components/dashboard/TopFiveChart/TopFiveChart";
+import AlbumTrendsChart from "@/components/dashboard/AlbumTrendsChart/AlbumTrendsChart";
+import { MEMBER_TYPE } from "@/types/enums/user.enum";
 
 const data = {
-  contents: [
+  tracks: [
     {
-      artist: {
-        memberId: 1,
-        koArtistName: "아이유",
-        enArtistName: "IU",
-      },
-      revenue: 1000000,
-      growthRate: 2.1,
-      proportion: 45,
+      trackId: 2,
+      koTrackName: "금요일에 만나요asdasdasd",
+      enTrackName: "meet on Friday",
+      monthlyTrend: [
+        {
+          month: 1,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 2,
+          settlement: 2231211,
+          revenue: 7323,
+        },
+        {
+          month: 3,
+          settlement: 2261211,
+          revenue: 732143,
+        },
+        {
+          month: 4,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 5,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 6,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 7,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 8,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 9,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 10,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 11,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 12,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+      ],
     },
     {
-      artist: {
-        memberId: 2,
-        koArtistName: "아이유",
-        enArtistName: "IU",
-      },
-      revenue: 1000000,
-      growthRate: 2.1,
-      proportion: 45,
-    },
-    {
-      artist: {
-        memberId: 3,
-        koArtistName: "아이유",
-        enArtistName: "IU",
-      },
-      revenue: 1000000,
-      growthRate: 2.1,
-      proportion: 45,
-    },
-    {
-      artist: {
-        memberId: 4,
-        koArtistName: "아이유",
-        enArtistName: "IU",
-      },
-      revenue: 1000000,
-      growthRate: 2.1,
-      proportion: 45,
-    },
-    {
-      artist: {
-        memberId: 5,
-        koArtistName: "아이유",
-        enArtistName: "IU",
-      },
-      revenue: 1000000,
-      growthRate: 2.1,
-      proportion: 45,
-    },
-    {
-      artist: {
-        memberId: 6,
-        koArtistName: "이름이 긴 아...",
-        enArtistName: "IU",
-      },
-      revenue: 1000000,
-      growthRate: 2.1,
-      proportion: 45,
+      trackId: 1,
+      koTrackName: "너랑나",
+      enTrackName: "U and I",
+      monthlyTrend: [
+        {
+          month: 1,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 2,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 3,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 4,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 5,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 6,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 7,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 8,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 9,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 10,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 11,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+        {
+          month: 12,
+          settlement: 2142344,
+          revenue: 732143,
+        },
+      ],
     },
   ],
 };
+
 const TonyPage = () => {
   return (
-    <TopFiveChart topFiveChartData={data} />
+    <AlbumTrendsChart albumTrendsChartData={data} memberType={MEMBER_TYPE.ADMIN} />
   );
 };
 
