f707878416593962cbd10621eda36947068a9daf
drizzle96 <dlxkffldk37@naver.com>
2023-08-19 22:25:43 +0900

    feat: 데이터가 없을 때 테이블 UI 구현

--- src/components/dashboard/TrackStatusTable/AdminTrackStatusTable.tsx
+++ src/components/dashboard/TrackStatusTable/AdminTrackStatusTable.tsx
@@ -12,9 +12,11 @@ import TableHeaderUI from "@/components/common/Table/Composition/TableHeaderUI";
 import TableRowUI from "@/components/common/Table/Composition/TableRowUI";
 import TooltipRoot from "@/components/common/Tooltip/TooltipRoot";
 import { ITrackTransaction } from "@/types/dto";
+import { DASHBOARD_TYPE } from "@/types/enums/dashboard.enum";
 import formatMoney from "@/utils/formatMoney";
 
 import styles from "./AdminTrackStatusTable.module.scss";
+import EmptyTableData from "./EmptyTableData";
 import { formatArtistCell } from "./TrackStatusTable.utils";
 
 const cx = classNames.bind(styles);
@@ -22,10 +24,13 @@ const cx = classNames.bind(styles);
 interface AdminTrackStatusTableProps {
   title: string
   data: ITrackTransaction[]
+  isEmpty?: boolean
   paginationElement?: React.ReactNode
 }
 
-const AdminTrackStatusTable = ({ title, data, paginationElement }: AdminTrackStatusTableProps) => {
+const AdminTrackStatusTable = ({
+  title, data, isEmpty = false, paginationElement,
+}: AdminTrackStatusTableProps) => {
   const handleClickSortByDropdown = (value: string) => {
     // TODO: 정렬 순서 쿼리 파람 변경
   };
@@ -56,51 +61,55 @@ const AdminTrackStatusTable = ({ title, data, paginationElement }: AdminTrackSta
           <SearchBar placeholder="검색어를 입력해주세요" theme="withSearchBar" onClick={handleClickSearchBar} />
         </div>
       </div>
-      <TableContainerUI
-        paginationElement={paginationElement}
-        stickyFirstCol
-        tableWidth={1200}
-      >
-        <TableHeaderUI>
-          <TableCellUI isHeader>곡명</TableCellUI>
-          <TableCellUI isHeader>앨범명</TableCellUI>
-          <TableCellUI isHeader>아티스트명</TableCellUI>
-          <TableCellUI isHeader>매출액</TableCellUI>
-          <TableCellUI isHeader>회사 이익</TableCellUI>
-          <TableCellUI isHeader>정산액</TableCellUI>
-          <TableCellUI isHeader align="left">요율</TableCellUI>
-        </TableHeaderUI>
-        <TableBodyUI>
-          {data.map((item) => {
-            return (
-              <TableRowUI key={item.track.trackId}>
-                <TableCellUI>
-                  <TooltipRoot message={item.track.koTrackName}>
-                    <p className={cx("ellipsis")}>{item.track.koTrackName}</p>
-                  </TooltipRoot>
-                </TableCellUI>
-                <TableCellUI>
-                  <TooltipRoot message={item.album.koAlbumName}>
-                    <p className={cx("ellipsis")}>{item.album.koAlbumName}</p>
-                  </TooltipRoot>
-                </TableCellUI>
-                <TableCellUI>
-                  <div>
-                    <TooltipRoot message={formatArtistCell(item.artists)}>
-                      <p className={cx("artistName", "ellipsis")}>{formatArtistCell(item.artists)}</p>
-                    </TooltipRoot>
-                    <p className={cx("artistName", "enName", "ellipsis")}>{item.artists[0].enArtistName}</p>
-                  </div>
-                </TableCellUI>
-                <TableCellUI>{formatMoney(item.revenue, "table")}</TableCellUI>
-                <TableCellUI>{formatMoney(item.netIncome, "table")}</TableCellUI>
-                <TableCellUI>{formatMoney(item.settlementAmount, "table")}</TableCellUI>
-                <TableCellUI align="left">{item.commissionRate && <ProgressBar value={item.commissionRate} />}</TableCellUI>
-              </TableRowUI>
-            );
-          })}
-        </TableBodyUI>
-      </TableContainerUI>
+      {isEmpty
+        ? <EmptyTableData type={DASHBOARD_TYPE.ADMIN} />
+        : (
+          <TableContainerUI
+            paginationElement={paginationElement}
+            stickyFirstCol
+            tableWidth={1200}
+          >
+            <TableHeaderUI>
+              <TableCellUI isHeader>곡명</TableCellUI>
+              <TableCellUI isHeader>앨범명</TableCellUI>
+              <TableCellUI isHeader>아티스트명</TableCellUI>
+              <TableCellUI isHeader>매출액</TableCellUI>
+              <TableCellUI isHeader>회사 이익</TableCellUI>
+              <TableCellUI isHeader>정산액</TableCellUI>
+              <TableCellUI isHeader align="left">요율</TableCellUI>
+            </TableHeaderUI>
+            <TableBodyUI>
+              {data.map((item) => {
+                return (
+                  <TableRowUI key={item.track.trackId}>
+                    <TableCellUI>
+                      <TooltipRoot message={item.track.koTrackName}>
+                        <p className={cx("ellipsis")}>{item.track.koTrackName}</p>
+                      </TooltipRoot>
+                    </TableCellUI>
+                    <TableCellUI>
+                      <TooltipRoot message={item.album.koAlbumName}>
+                        <p className={cx("ellipsis")}>{item.album.koAlbumName}</p>
+                      </TooltipRoot>
+                    </TableCellUI>
+                    <TableCellUI>
+                      <div>
+                        <TooltipRoot message={formatArtistCell(item.artists)}>
+                          <p className={cx("artistName", "ellipsis")}>{formatArtistCell(item.artists)}</p>
+                        </TooltipRoot>
+                        <p className={cx("artistName", "enName", "ellipsis")}>{item.artists[0].enArtistName}</p>
+                      </div>
+                    </TableCellUI>
+                    <TableCellUI>{formatMoney(item.revenue, "table")}</TableCellUI>
+                    <TableCellUI>{formatMoney(item.netIncome, "table")}</TableCellUI>
+                    <TableCellUI>{formatMoney(item.settlementAmount, "table")}</TableCellUI>
+                    <TableCellUI align="left">{item.commissionRate && <ProgressBar value={item.commissionRate} />}</TableCellUI>
+                  </TableRowUI>
+                );
+              })}
+            </TableBodyUI>
+          </TableContainerUI>
+        )}
     </section>
   );
 };
