c95187ca693c610ec637b007614bb6a0d523d7a7
SeyoungCho <wnl383@naver.com>
2023-08-19 21:47:38 +0900

    doc: generateRandomStringWithRegex 유틸함수jsDoc 추가

--- src/pages/admin/artists/new/index.utils.ts
+++ src/pages/admin/artists/new/index.utils.ts
@@ -1,3 +1,11 @@
+/**
+ * 정규표현식과 최소, 최대 길이를 받아 랜덤 문자열을 생성하는 유틸 함수
+ * @author [SeyoungCho](https://github.com/seyoungcho)
+ * @param regex {RegExp} 정규표현식
+ * @param minLength {number} 문자열 최소 길이
+ * @param maxLength {number} 문자열 최대 길이
+ * @returns {string} 정규 표현식과 최소, 최대 길이에 맞는 랜덤 스트링 반환
+ */
 export const generateRandomStringWithRegex = (
   regex: RegExp,
   minLength: number,
