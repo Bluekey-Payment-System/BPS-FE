4a3fd9b6cc621fd31056c8cd27ba76531a48c450
SeyoungCho <wnl383@naver.com>
2023-08-19 21:48:08 +0900

    feat: 아티스트 등록 폼에 validation 로직 추가

--- src/pages/admin/artists/new/index.page.tsx
+++ src/pages/admin/artists/new/index.page.tsx
@@ -1,3 +1,6 @@
+import { ChangeEvent } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
 import classNames from "classnames/bind";
 
 import Button from "@/components/common/CommonBtns/Button/Button";
@@ -11,10 +14,48 @@ import MainLayout from "@/components/common/Layouts/MainLayout";
 import SectionHr from "@/components/common/Layouts/SectionHr";
 
 import styles from "./index.module.scss";
+import { generateRandomStringWithRegex } from "./index.utils";
 
 const cx = classNames.bind(styles);
 
+interface CreateArtistFieldValues {
+  email: string,
+  loginId: string,
+  name: string,
+  enName: string,
+  password: string,
+  profileImage?: File,
+  commissionRate?: number,
+  sameKoNameWithEnName: boolean,
+}
+
 const ArtistCreatePage = () => {
+  const {
+    register, handleSubmit, formState: { errors }, setValue, watch,
+  } = useForm<CreateArtistFieldValues>({
+    mode: "onBlur",
+    defaultValues: {
+      password: generateRandomStringWithRegex(/^[a-zA-Z0-9!@#$%^&*()-_+=<>?]*$/, 6, 18),
+      sameKoNameWithEnName: false,
+    },
+  });
+  // 활동 예명(한글) 필드를 수동으로 영문과 동일하게 입력하는 경우, '영문과 동일' 체크박스 자동 체크
+  const handleBlureNameInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const targetValue = e.target.value;
+    const targetName = e.target.name;
+    const compareName = targetName === "name" ? "enName" : "name";
+    if (targetValue === watch(compareName)) {
+      setValue("sameKoNameWithEnName", true);
+      return;
+    }
+    setValue("sameKoNameWithEnName", false);
+  };
+  const onSubmit: SubmitHandler<CreateArtistFieldValues> = (data) => {
+    // TODO: /api/v1/artist 로 POST요청 (Content-Type: multipart/formData)
+    // eslint-disable-next-line no-console
+    console.log(JSON.stringify(data));
+  };
+
   return (
     <MainLayout title="아티스트 등록">
       <ArtboardLayout>
@@ -22,24 +63,104 @@ const ArtistCreatePage = () => {
           <section className={cx("profileImageSection")}>
             <h1 className={cx("title")}>아티스트 프로필 이미지 업로드</h1>
             <div className={cx("imageUploadContainer")}>
-              <ImageUploader shape="circle" />
+              <ImageUploader shape="circle" {...register("profileImage")} />
               <span className={cx("sizeLimitText")}>*이미지 크기는 6MB 이하로 업로드 해주세요.</span>
             </div>
           </section>
           <SectionHr />
           <section className={cx("artistInfoSection")}>
             <h1 className={cx("title")}>아티스트 계정 정보 입력</h1>
-            <form className={cx("form")}>
+            {/* eslint-disable @typescript-eslint/no-misused-promises */}
+            <form className={cx("form")} onSubmit={handleSubmit(onSubmit)}>
               <div className={cx("checkboxContainer")}>
-                <Checkbox label="영문과 동일" />
+                <Checkbox
+                  label="영문과 동일"
+                  {...register("sameKoNameWithEnName", {
+                    onChange: (e: ChangeEvent<HTMLInputElement>) => {
+                      if (e.target.checked) {
+                        setValue("name", watch("enName"), { shouldTouch: true, shouldDirty: true });
+                      }
+                    },
+                  })}
+                />
               </div>
-              <TextField label="활동 예명(한글)" errors={{}} />
-              <TextField label="활동 예명(영문)" errors={{}} />
-              <TextField label="계정 아이디" errors={{}} />
-              <TextField label="계정 이메일" errors={{}} />
-              <TextFieldWithCopy label="임시 비밀번호" bottomText="*분실시 아티스트 계정 관리 페이지에서 재발급 받을 수 있습니다." errors={{}} />
-              <TextFieldWithUnit label="기본 요율" unit="%" errors={{}} />
-              <Button size="large" theme="dark" style={{ width: "218px", marginTop: "16px" }}>아티스트 등록</Button>
+              <TextField
+                label="*활동 예명(한글)"
+                {...register("name", {
+                  required: "예명을 입력하세요.",
+                  onBlur: handleBlureNameInput,
+                })}
+                disabled={watch("sameKoNameWithEnName")}
+                errors={errors}
+              />
+              <TextField
+                label="*활동 예명(영문)"
+                {...register("enName", {
+                  required: "영문 예명을 입력하세요.",
+                  onBlur: handleBlureNameInput,
+                })}
+                errors={errors}
+              />
+              <TextField
+                label="*계정 아이디"
+                {...register("loginId", {
+                  required: "*계정 아이디를 입력하세요.",
+                  minLength: {
+                    value: 5,
+                    message: "*아이디는 5자 이상으로 입력해주세요.",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "*아이디는 20자 이하로 입력해주세요.",
+                  },
+                  pattern: {
+                    value: /[-_a-z0-9]$/,
+                    message: "*영문 소문자, 숫자, 특수기호(_),(-)만 사용 가능합니다.",
+                  },
+                  validate: {
+                    startsWithChar: (v: string) => { return /^[a-z]/g.test(v) || "*아이디의 첫 글자는 영문 소문자로 시작해야 합니다."; },
+                  },
+                })}
+                errors={errors}
+                placeholder="5~20자의 영문 소문자, 숫자, 특수기호(_),(-) 사용"
+              />
+              <TextField
+                label="계정 이메일"
+                {...register("email", {
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g,
+                    message: "*올바른 이메일을 입력해주세요.",
+                  },
+                })}
+                errors={errors}
+                bottomText="*해당 이메일로 정산 완료 메일이 발송됩니다."
+              />
+              <TextFieldWithCopy
+                label="임시 비밀번호"
+                {...register("password")}
+                disabled
+                bottomText="*분실시 아티스트 계정 관리 페이지에서 재발급 받을 수 있습니다."
+                errors={errors}
+              />
+              <TextFieldWithUnit
+                label="기본 요율(아티스트 측)"
+                {...register("commissionRate", {
+                  valueAsNumber: true,
+                  validate: (v) => { return !(v && Number.isNaN(v)) || "*숫자를 입력하세요"; },
+                  min: {
+                    value: 0,
+                    message: "*0~100 사이의 값을 입력하세요.",
+                  },
+                  max: {
+                    value: 100,
+                    message: "*0~100 사이의 값을 입력하세요.",
+                  },
+                })}
+                unit="%"
+                bottomText="*트랙별로 기본 요율과 다른 요율을 적용할 수도 있습니다."
+                errors={errors}
+              />
+              <Button type="submit" size="large" theme="dark" style={{ width: "218px", marginTop: "16px" }}>아티스트 등록</Button>
             </form>
           </section>
         </div>
