b2202efcfe23dc5b9c937225bad67fd3bf824e51
SeyoungCho <wnl383@naver.com>
2023-08-20 19:24:01 +0900

    feat: 앨범 등록페이지에 등록 폼 AlbumForm 컴포넌트로 교체

--- src/pages/admin/albums/new/index.page.tsx
+++ src/pages/admin/albums/new/index.page.tsx
@@ -1,57 +1,43 @@
-import { useState } from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 
 import classNames from "classnames/bind";
 
-import Button from "@/components/common/CommonBtns/Button/Button";
-import Dropdown from "@/components/common/Dropdown/Dropdown";
+import AlbumForm from "@/components/album/AlbumForm/AlbumForm";
 import ImageUploader from "@/components/common/ImageUploader/ImageUploader";
-import TextField from "@/components/common/Inputs/TextField/TextField";
 import ArtboardLayout from "@/components/common/Layouts/ArtboardLayout";
 import MainLayout from "@/components/common/Layouts/MainLayout";
 import SectionHr from "@/components/common/Layouts/SectionHr";
 import SectionLayout from "@/components/common/Layouts/SectionLayout";
-import Spacing from "@/components/common/Layouts/Spacing";
+import { IAlbumFieldValues } from "@/types/album.types";
 
 import styles from "./index.module.scss";
 
 const cx = classNames.bind(styles);
 
 const AlbumCreatePage = () => {
-  const [artist, setArtist] = useState("");
+  const methods = useForm<IAlbumFieldValues>({ mode: "onBlur" });
+  const onSubmit:SubmitHandler<IAlbumFieldValues> = (data) => {
+    // TODO: /api/v1/albums 로 POST mutation 훅 사용
+    // eslint-disable-next-line no-console
+    console.log(data);
+  };
   return (
     <MainLayout title="앨범 등록">
       <ArtboardLayout>
-        <div className={cx("container")}>
-          <SectionLayout title="앨범 아트 업로드">
-            <div className={cx("imageUploadContainer")}>
-              <ImageUploader shape="square" />
-              <span className={cx("sizeLimitText")}>*이미지 크기는 6MB 이하로 업로드 해주세요.</span>
-            </div>
-          </SectionLayout>
-          <SectionHr />
-          <SectionLayout title="앨범 정보 입력">
-            <form className={cx("form")}>
-              <TextField label="*앨범명 (한글)" errors={{}} />
-              <TextField label="*앨범명 (영문)" errors={{}} />
-              <div className={cx("dropdownContainer")}>
-                <span>대표 아티스트</span>
-                <Dropdown
-                  theme="hasSearchBar"
-                  hasSearchBar
-                  dropdownListData={[
-                    // TODO: db에 있는 모든 아티스트 names를 가져와서(api) 여기에 뿌리기
-                    "혁기", "지미가드너", "53x", "송민섭",
-                  ]}
-                  onClick={(value) => { setArtist(value); }}
-                />
-                <input name="artist" type="hidden" value={artist} />
-                <Spacing size={14} />
+        <FormProvider {...methods}>
+          <div className={cx("container")}>
+            <SectionLayout title="앨범 아트 업로드">
+              <div>
+                <ImageUploader shape="square" {...methods.register("albumImage")} />
+                <span className={cx("sizeLimitText")}>*이미지 크기는 6MB 이하로 업로드 해주세요.</span>
               </div>
-              <Spacing size={0} />
-              <Button theme="dark" size="large" type="submit" style={{ marginTop: "26px", width: "218px" }}>앨범 등록하기</Button>
-            </form>
-          </SectionLayout>
-        </div>
+            </SectionLayout>
+            <SectionHr />
+            <SectionLayout title="앨범 정보 입력">
+              <AlbumForm submitBtnText="앨범 등록하기" onSubmit={onSubmit} />
+            </SectionLayout>
+          </div>
+        </FormProvider>
       </ArtboardLayout>
     </MainLayout>
   );
