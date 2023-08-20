bd09b2522db3ada71fdba9a2a7d080821c998e94
drizzle96 <dlxkffldk37@naver.com>
2023-08-20 21:03:38 +0900

    feat: 에러 페이지 UI 제작

---
+++ src/pages/_error.page.tsx
@@ -0,0 +1,24 @@
+import Button from "@/components/common/CommonBtns/Button/Button";
+import FallbackPageLayout from "@/components/layout/FallbackPageLayout";
+
+const Error = () => {
+  // TODO: 에러 상태 코드 및 메시지 받기
+  const statusCode = 500;
+  const errorMessage = "서버 오류가 발생했습니다.";
+
+  return (
+    <FallbackPageLayout
+      pageType="error"
+      descriptions={[[`${statusCode}: ${errorMessage}`]]}
+      buttonElements={(
+        // TODO: 버튼 핸들러 달기
+        <>
+          <Button size="medium" theme="dark" onClick={() => { }}>홈(대시보드)으로 가기</Button>
+          <Button size="medium" theme="dark" onClick={() => { }}>새로고침</Button>
+        </>
+      )}
+    />
+  );
+};
+
+export default Error;
