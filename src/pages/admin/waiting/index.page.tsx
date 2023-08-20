75ecbcda8ab14e86c61ba52b314ff6461e4d94b5
drizzle96 <dlxkffldk37@naver.com>
2023-08-20 21:16:53 +0900

    feat: 관리자 권한 승인 대기/거절 페이지 UI 제작

---
+++ src/pages/admin/waiting/index.page.tsx
@@ -0,0 +1,20 @@
+import Button from "@/components/common/CommonBtns/Button/Button";
+import FallbackPageLayout from "@/components/layout/FallbackPageLayout";
+
+const Error = () => {
+  return (
+    <FallbackPageLayout
+      pageType="approvalRejected"
+      descriptions={[[
+        "권한을 재요청하여 일주일 내로 승인되지 않을 시,",
+        "자동으로 회원정보가 삭제됩니다.",
+      ]]}
+      buttonElements={(
+        // TODO: 버튼 핸들러 달기
+        <Button size="large" theme="bright" onClick={() => { }}>관리자 권한 재요청하기</Button>
+      )}
+    />
+  );
+};
+
+export default Error;
