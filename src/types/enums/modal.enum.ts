0bece114f9ac7febab6606ffc1a296eed8f35828
drizzle96 <dlxkffldk37@naver.com>
2023-08-21 00:21:18 +0900

    feat: 모달 타입으로 ALBUM_INFO 추가

--- src/types/enums/modal.enum.ts
+++ src/types/enums/modal.enum.ts
@@ -2,6 +2,7 @@ export const MODAL_TYPE = {
   ERROR: "ERROR",
   CONFIRM: "CONFIRM",
   FORM: "FORM",
+  ALBUM_INFO: "ALBUM_INFO",
 } as const;
 
 export type ModalType = (typeof MODAL_TYPE)[keyof typeof MODAL_TYPE];
