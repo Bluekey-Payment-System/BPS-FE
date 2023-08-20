bb165845ca5adb4e3fa4672b877b73002fc43c76
hayoung-99 <nhy990130@gmail.com>
2023-08-21 02:46:58 +0900

    feat: ssr 업로드 내역 fetch

--- src/pages/admin/upload-revenue/[[...monthYear]].page.tsx
+++ src/pages/admin/upload-revenue/[[...monthYear]].page.tsx
@@ -1,3 +1,7 @@
+import {
+  QueryClient, UseQueryResult, dehydrate, useMutation, useQuery, useQueryClient,
+} from "@tanstack/react-query";
+import { GetServerSideProps } from "next";
 // import { useRouter } from "next/router";
 
 import ExcelFileUploader from "@/components/common/ExcelFileUploader/ExcelFileUploader";
@@ -8,8 +12,64 @@ import SectionLayout from "@/components/common/Layouts/SectionLayout";
 import MonthPickerDropdown from "@/components/common/MonthPicker/MonthPickerDropdown";
 import UploadHistroyTable from "@/components/uploadRevenue/UploadHistoryTable/UploadHistoryTable";
 import { MOCK_TRANSACTION_UPLOAD } from "@/constants/mock";
+import { IGETTransactionUploadResponse } from "@/services/api/types/transaction";
+import { MEMBER_TYPE } from "@/types/enums/user.enum";
+
+const getServerSideProps: GetServerSideProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    [MEMBER_TYPE.ADMIN, "settlement-upload-history"],
+    () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(MOCK_TRANSACTION_UPLOAD);
+        }, 3000);
+      });
+    },
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
 
 const UploadRevenuePage = () => {
+  // const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const { data }: UseQueryResult<IGETTransactionUploadResponse> = useQuery(
+    [MEMBER_TYPE.ADMIN, "settlement-upload-history"],
+    () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(MOCK_TRANSACTION_UPLOAD);
+        }, 3000);
+      });
+    },
+    {
+      staleTime: Infinity,
+    },
+  );
+
+  const mutationTest = useMutation(() => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Mutation Success!");
+      }, 2000);
+    });
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries([MEMBER_TYPE.ADMIN, "settlement-upload-history"]);
+    },
+  });
+
+  const handleMutationTest = () => {
+    mutationTest.mutate();
+  };
+
   return (
     <MainLayoutWithDropdown
       title="정산 내역 업로드"
@@ -17,6 +77,7 @@ const UploadRevenuePage = () => {
         <MonthPickerDropdown />
     )}
     >
+      <button type="button" onClick={handleMutationTest}>클릭!</button>
       <ArtboardLayout>
         <div style={{ width: 730 }}>
           <SectionLayout title="정산 내역 파일 업로드">
@@ -24,7 +85,7 @@ const UploadRevenuePage = () => {
           </SectionLayout>
           <SectionHr isThick />
           <SectionLayout title="업로드 내역">
-            <UploadHistroyTable uploadList={MOCK_TRANSACTION_UPLOAD.contents} />
+            <UploadHistroyTable uploadList={data?.contents} />
           </SectionLayout>
         </div>
       </ArtboardLayout>
@@ -32,4 +93,5 @@ const UploadRevenuePage = () => {
   );
 };
 
+export { getServerSideProps };
 export default UploadRevenuePage;
