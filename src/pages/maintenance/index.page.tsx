aadb7162b3a8f02a1cb0b61296528a1567d8c6e1
drizzle96 <dlxkffldk37@naver.com>
2023-08-21 16:26:09 +0900

    rename: 변수명 수정

--- src/pages/maintenance/index.page.tsx
+++ src/pages/maintenance/index.page.tsx
@@ -2,9 +2,9 @@ import FallbackPageLayout from "@/components/layout/FallbackPageLayout";
 
 const MainTenance = () => {
   // TODO: 점검 시간, 이유 반영
-  const checkingDate = "2023/8/20";
-  const checkingTime = "오후 4:00 ~ 오후 8:00";
-  const checkingReason = "서비스 긴급점검";
+  const date = "2023/8/20";
+  const duration = "오후 4:00 ~ 오후 8:00";
+  const purpose = "서비스 긴급점검";
   return (
     <FallbackPageLayout
       pageType="systemChecking"
@@ -13,9 +13,9 @@ const MainTenance = () => {
         "이용에 불편을 드린 점 진심으로 사과 드리며,",
         "예정된 시간 내에 정상적으로 서비스를 재개할 수 있도록 최선을 다하겠습니다.",
       ], [
-        `일자: ${checkingDate}`,
-        `시간: ${checkingTime}`,
-        `사유: ${checkingReason}`,
+        `일자: ${date}`,
+        `시간: ${duration}`,
+        `사유: ${purpose}`,
       ]]}
     />
   );
