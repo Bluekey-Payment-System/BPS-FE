ae2507a64df18109915d49af5439d7a1641752de
drizzle96 <dlxkffldk37@naver.com>
2023-08-21 17:58:14 +0900

    feat: type의 각 obj를 상수로 제작

---
+++ src/constants/fallbackPageLayout.ts
@@ -0,0 +1,49 @@
+export const NOTFOUND_TYPE_OBJ = {
+  image: {
+    src: "/images/error-warning.svg",
+    width: 166,
+    height: 170,
+    alt: "에러 워닝",
+  },
+  title: "죄송합니다. 원하시는 페이지를 찾을 수 없습니다.",
+};
+
+export const ERROR_TYPE_OBJ = {
+  image: {
+    src: "/images/error-warning.svg",
+    width: 166,
+    height: 170,
+    alt: "에러 워닝",
+  },
+  title: "잠시후 다시 확인해주세요.",
+};
+
+export const MAINTENANCE_TYPE_OBJ = {
+  image: {
+    src: "/images/maintenance.png",
+    width: 155,
+    height: 170,
+    alt: "시스템 점검",
+  },
+  title: "시스템 점검 중입니다.",
+};
+
+export const APPROVAL_PENDING_TYPE_OBJ = {
+  image: {
+    src: "/images/pending.png",
+    width: 138,
+    height: 170,
+    alt: "신청 대기 중",
+  },
+  title: "관리자 권한 승인 대기중입니다.",
+};
+
+export const APPROVAL_REJECTED_TYPE_OBJ = {
+  image: {
+    src: "/images/rejected.png",
+    width: 187,
+    height: 170,
+    alt: "신청 거절",
+  },
+  title: "관리자 권한 신청이 거절되었습니다.",
+};
