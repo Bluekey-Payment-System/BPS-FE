385fa77a063ae7fcef824ee89c94c83b0bf40c86
drizzle96 <dlxkffldk37@naver.com>
2023-08-21 18:02:30 +0900

    fix: 잘못된 prop 값 수정

--- src/pages/404.page.tsx
+++ src/pages/404.page.tsx
@@ -4,7 +4,7 @@ import FallbackPageLayout from "@/components/layout/FallbackPageLayout";
 const Custom404 = () => {
   return (
     <FallbackPageLayout
-      pageType="404"
+      pageType="notFound"
       description={`페이지의 주소가 잘못 입력되었거나,
         주소가 변경 혹은 삭제되어 현재 페이지를 찾을 수 없습니다.
         주소를 다시 확인해주세요.`}
