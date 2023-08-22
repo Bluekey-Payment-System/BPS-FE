d20e12a03f34d180c072580559a7c7b2f4ef1e8e
hayoung-99 <nhy990130@gmail.com>
2023-08-22 17:39:29 +0900

    feat: percentage가 양수인 경우 '+' prefix 추가

--- src/components/common/Chip/Chip.tsx
+++ src/components/common/Chip/Chip.tsx
@@ -7,23 +7,33 @@ type Fluctuation = "increase" | "decrease" | "same";
 const cx = classNames.bind(styles);
 
 /**
+ * 숫자를 받아 상승/감소율 Chip을 리턴합니다. null의 경우 '-%'로 변환됩니다.
  * @author [hayoung-99](https://github.com/hayoung-99)
- * @param percentage 백분율 값
+ * @param {number | null} percentage 백분율 값
  * @returns 백분율을 나타내는 Chip 컴포넌트
  */
 const Chip = ({ percentage }: { percentage: number | null }) => {
   let fluctuation: Fluctuation;
 
-  if (!percentage) {
+  let percentageToString: string;
+  if (percentage === null) {
+    percentageToString = "- ";
     fluctuation = "same";
-  } else if (percentage > 0) {
-    fluctuation = "increase";
   } else {
-    fluctuation = "decrease";
+    percentageToString = percentage.toString();
+
+    if (percentage === 0) {
+      fluctuation = "same";
+    } else if (percentage > 0) {
+      percentageToString = `+${percentageToString}`;
+      fluctuation = "increase";
+    } else {
+      fluctuation = "decrease";
+    }
   }
 
   return (
-    <div className={cx("chipBox", fluctuation)}>{percentage ? `${percentage}%` : "0%"}</div>
+    <div className={cx("chipBox", fluctuation)}>{`${percentageToString}%`}</div>
   );
 };
 
