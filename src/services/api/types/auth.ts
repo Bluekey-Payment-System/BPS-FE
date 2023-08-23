4ebc799f94347a7a249d9e4ab708afb3a1fedeab
drizzle96 <dlxkffldk37@naver.com>
2023-08-23 20:15:38 +0900

    feat: Auth api 함수 제작
    Co-authored-by: hayoung-99 <hayoung-99@users.noreply.github.com>

--- src/services/api/types/auth.ts
+++ src/services/api/types/auth.ts
@@ -1,4 +1,5 @@
-import { IAdminSignup, ISignIn } from "@/types/dto";
+import { IAdminProfile, IAdminSignup, ISignIn } from "@/types/dto";
+import { AdminType, ArtistType } from "@/types/enums/user.enum";
 
 export interface IPostAdminSignInRequest extends ISignIn {
 }
@@ -6,11 +7,46 @@ export interface IPostAdminSignInRequest extends ISignIn {
 export interface IPostAdminSignUpRequest extends IAdminSignup {
 }
 
-export interface IPostMemberSignInRequest extends ISignIn {
+export interface IPostArtistSignInRequest extends ISignIn {
 }
 
-export interface IPatchChangePassword extends Omit<ISignIn, "loginId"> {
+export interface IPatchChangePasswordRequest extends Omit<ISignIn, "loginId"> {
 }
 
 export interface IPostConfirmPasswordRequest extends Omit<ISignIn, "loginId"> {
+}
+
+interface ICommonSignInInfo {
+  id: number,
+  loginId: string,
+  profileImage: string | null,
+}
+
+interface IAdminSignInInfo extends ICommonSignInInfo {
+  email: string,
+  type: "ADMIN" | "USER" // 기존의 MEMBER_TYPE과의 관계가 불분명한 것 같고 사용하는 쪽이 아직 없어 enum 처리 안함
+  role: AdminType,
+}
+
+interface IArtistSignInInfo extends ICommonSignInInfo {
+  email: string,
+  type: "ADMIN" | "USER"
+  role: ArtistType,
+}
+
+export interface IPostAdminSignInResponse {
+  member: IAdminSignInInfo,
+  jwtInformation: {
+    accessToken: string
+  }
+}
+
+export interface IPostArtistSignInResponse {
+  member: IArtistSignInInfo
+  jwtInformation: {
+    accessToken: string
+  }
+}
+
+export interface IPostAdminSignUpResponse extends Omit<IAdminProfile, "profileImage"> {
 }
