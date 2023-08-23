46b39d91d006389e3416bc2f93d1f95ab6f0d49c
SeyoungCho <wnl383@naver.com>
2023-08-23 20:33:02 +0900

    doc: ChangePasswordForm 컴포넌트 jsDoc 추가

--- src/components/auth/ChangePasswordForm/ChangePasswordForm.tsx
+++ src/components/auth/ChangePasswordForm/ChangePasswordForm.tsx
@@ -7,7 +7,19 @@ import CurrentPasswordForm from "./CurrentPasswordForm/CurrentPasswordForm";
 import NewPasswordForm from "./NewPasswordForm/NewPasswordForm";
 
 const cx = classNames.bind(styles);
-
+/**
+ * 비밀번호 변경 폼
+ * @author [SeyoungCho](https://github.com/seyoungcho)
+ * @param {Fucntion} onComplete 비밀번호 변경 모든 단계를 완료한 뒤 실행할 함수
+ * @example
+ * ```tsx
+ * const [showModal, setShowModal] = useState(false);
+ * ...
+ * <Modal isOpen={showModal}>
+ *   <ChangePasswordForm onComplete={()=>{alert("비밀번호 변경 완료!"); setShowModal(false)}} />
+ * </Modal>
+ * ```
+ */
 const ChangePasswordForm = ({ onComplete }: { onComplete: () => void }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
