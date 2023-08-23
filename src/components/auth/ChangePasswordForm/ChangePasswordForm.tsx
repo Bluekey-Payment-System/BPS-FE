ce5f48d1b4d119627cec549c62177622e72fda7e
SeyoungCho <wnl383@naver.com>
2023-08-23 18:53:57 +0900

    feat: 비밀번호 재설정 폼 컴포넌트 퍼블리싱

---
+++ src/components/auth/ChangePasswordForm/ChangePasswordForm.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import classNames from "classnames/bind";
+
+import styles from "./ChangePasswordForm.module.scss";
+import CurrentPasswordForm from "./CurrentPasswordForm/CurrentPasswordForm";
+import NewPasswordForm from "./NewPasswordForm/NewPasswordForm";
+
+const cx = classNames.bind(styles);
+
+const ChangePasswordForm = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  return (
+    <div className={cx("container")}>
+      <h1 className={cx("title")}>비밀번호 재설정</h1>
+      {currentPage === 1 ? (
+        <CurrentPasswordForm
+          onSuccess={() => { setCurrentPage((prev) => { return prev + 1; }); }}
+        />
+      ) : <NewPasswordForm />}
+    </div>
+  );
+};
+
+export default ChangePasswordForm;
