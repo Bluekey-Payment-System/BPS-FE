1ae498883d7a02f63418b6a6effce38c36f0b5c7
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-23 19:09:58 +0900

    docs: formatDropdownList jsdoc 제작

--- src/utils/formatDropdownList.ts
+++ src/utils/formatDropdownList.ts
@@ -4,6 +4,11 @@ interface IFormatDropdownList {
   [key: string]: string | number
 }
 
+/**
+ * @author 임병욱
+ * @param {IFormatDropdownList} list - 드롭다운에서 사용할 리스트 ex)[{id: number, name: string},]
+ * @return {IHasSearchBarData} formattedList - 가공된 리스트 [{id: string, name: string},]
+ */
 const formatDropdownList = (list: IFormatDropdownList[]): IHasSearchBarData[] => {
   const formattedList = list.map((item) => {
     return {
