0ab3cae4cd3a160e98146c37bf4b2e9b6cf372db
drizzle96 <dlxkffldk37@naver.com>
2023-08-23 17:14:59 +0900

    feat: instance request 생성
    Co-authored-by: hayoung-99 <hayoung-99@users.noreply.github.com>

---
+++ src/services/api/requests/requests.api.ts
@@ -0,0 +1,57 @@
+import {
+  AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig,
+} from "axios";
+
+import { instance } from "../instance";
+
+/* get 요청 */
+export const getRequest = async <T>(
+  url: string,
+  config?: AxiosRequestConfig,
+): Promise<T> => {
+  const response = await instance.get<T>(
+    url,
+    config as InternalAxiosRequestConfig,
+  );
+  return response.data;
+};
+
+// /* post 요청 */
+export const postRequest = async <T, D>(
+  url: string,
+  data: D,
+  config?: AxiosRequestConfig,
+): Promise<T> => {
+  const response = await instance.post<T, AxiosResponse<T>, D>(
+    url,
+    data,
+    config as InternalAxiosRequestConfig,
+  );
+  return response.data;
+};
+
+/* delete 요청 */
+export const deleteRequest = async <T>(
+  url: string,
+  config?: AxiosRequestConfig,
+): Promise<T> => {
+  const response = await instance.delete<T>(
+    url,
+    config as InternalAxiosRequestConfig,
+  );
+  return response.data;
+};
+
+/* patch 요청 */
+export const patchRequest = async <T, D>(
+  url: string,
+  data: D,
+  config?: AxiosRequestConfig,
+): Promise<T> => {
+  const response = await instance.patch<T, AxiosResponse<T>, D>(
+    url,
+    data,
+    config as InternalAxiosRequestConfig,
+  );
+  return response.data;
+};
