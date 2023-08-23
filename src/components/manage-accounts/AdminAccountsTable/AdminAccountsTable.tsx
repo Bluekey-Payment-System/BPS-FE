0a85ce68ab6fafd0055f80d19265d8dff8fa3833
drizzle96 <dlxkffldk37@naver.com>
2023-08-23 14:50:49 +0900

    feat: 비밀번호 재발급 시 재발급 모달과 연결

--- src/components/manage-accounts/AdminAccountsTable/AdminAccountsTable.tsx
+++ src/components/manage-accounts/AdminAccountsTable/AdminAccountsTable.tsx
@@ -11,8 +11,12 @@ import TableRowUI from "@/components/common/Table/Composition/TableRowUI";
 import TooltipRoot from "@/components/common/Tooltip/TooltipRoot";
 import useAlertModal from "@/hooks/useAlertModal";
 import useToast from "@/hooks/useToast";
+// TODO: 공통 utils로 위치 변경?
+import { generateRandomStringWithRegex } from "@/pages/admin/artists/new/index.utils";
 import { IAdminAccount } from "@/types/dto";
 import { MODAL_TYPE } from "@/types/enums/modal.enum";
+
+import ReissuedPasswordModal from "../PasswordReissueModal/ReissuedPasswordModal";
 
 import styles from "./AdminAccountsTable.module.scss";
 
@@ -31,8 +35,10 @@ interface AdminAccountsTableProps {
 
 const AdminAccountsTable = ({ accounts, paginationElement }: AdminAccountsTableProps) => {
   const [focusedAccount, setFocusedAccount] = useState<IFocusedAccount>();
-  const [isOpenDeleteAccModal, setIsOpenDeleteAccModal] = useState(false);
-  const [isOpenReissuePwModal, setIsOpenReissuePwModal] = useState(false);
+  const [isOpenDeleteAlertModal, setIsOpenDeleteAlertModal] = useState(false);
+  const [isOpenReissueAlertModal, setIsOpenReissueAlertModal] = useState(false);
+  const [isOpenReissuedPwModal, setIsOpenReissuedPwModal] = useState(false);
+  const [newPassword, setNewPassword] = useState<string>();
   const { showToast } = useToast();
   const handleDeleteAccount = (memberId: number, nickName: string) => {
     // TODO: 계정 delete api 달기
@@ -41,17 +47,19 @@ const AdminAccountsTable = ({ accounts, paginationElement }: AdminAccountsTableP
   };
 
   const handleReissuePassword = (memberId: number, nickName: string) => {
-    // TODO: 모달
+    setNewPassword(generateRandomStringWithRegex(/^[a-zA-Z0-9@$!%*?&_-]*$/, 6, 18));
+    // TODO: 계정 pw 변경 api 달기
     showToast(`“${nickName}" 계정의 비밀번호가 재발급 되었습니다.`);
     setFocusedAccount(undefined);
+    setIsOpenReissuedPwModal(true);
   };
 
   const deleteAlertModalProps = {
-    open: isOpenDeleteAccModal,
+    open: isOpenDeleteAlertModal,
     type: MODAL_TYPE.CONFIRM,
     title: "계정 탈퇴",
     message: "해당 어드민 계정 ID를 삭제 하시겠습니까?",
-    onClose: () => { setIsOpenDeleteAccModal(false); },
+    onClose: () => { setIsOpenDeleteAlertModal(false); },
     onClickProceed: () => {
       handleDeleteAccount(focusedAccount!.memberId, focusedAccount!.nickName);
     },
@@ -59,11 +67,11 @@ const AdminAccountsTable = ({ accounts, paginationElement }: AdminAccountsTableP
     closeBtnText: "아니요",
   };
   const reissueAlertModalProps = {
-    open: isOpenReissuePwModal,
+    open: isOpenReissueAlertModal,
     type: MODAL_TYPE.CONFIRM,
     title: "비밀번호 재발급",
     message: `“${focusedAccount?.nickName}” 계정의 비밀번호를 재발급 하시겠습니까?`,
-    onClose: () => { setIsOpenReissuePwModal(false); },
+    onClose: () => { setIsOpenReissueAlertModal(false); },
     onClickProceed: () => {
       handleReissuePassword(focusedAccount!.memberId, focusedAccount!.nickName);
     },
@@ -76,63 +84,70 @@ const AdminAccountsTable = ({ accounts, paginationElement }: AdminAccountsTableP
   useEffect(() => {
     if (focusedAccount) {
       if (focusedAccount.target === "reissue") showReissueAlertModal();
-      else showDeleteAlertModal();
+      if (focusedAccount.target === "delete") showDeleteAlertModal();
     }
   }, [focusedAccount, showDeleteAlertModal, showReissueAlertModal]);
 
   return (
-    <TableContainerUI
-      stickyLastCol
-      tableWidth={1110}
-      paginationElement={paginationElement}
-    >
-      <TableHeaderUI>
-        <TableCellUI isHeader>닉네임</TableCellUI>
-        <TableCellUI isHeader>계정 ID</TableCellUI>
-        {/* // TODO: 어드민 타입? */}
-        <TableCellUI isHeader colWidth={330}>계정 이메일</TableCellUI>
-        <TableCellUI isHeader colWidth={232}>비고</TableCellUI>
-      </TableHeaderUI>
-      <TableBodyUI>
-        {accounts.map((account) => {
-          return (
-            <TableRowUI key={account.memberId}>
-              <TableCellUI>
-                <TooltipRoot message={account.nickName}>
-                  <p className={cx("ellipsis")}>{account.nickName}</p>
-                </TooltipRoot>
-              </TableCellUI>
-              <TableCellUI>
-                <TooltipRoot message={account.loginId}>
-                  <p className={cx("ellipsis")}>{account.loginId}</p>
-                </TooltipRoot>
-              </TableCellUI>
-              <TableCellUI>
-                <TooltipRoot message={account.email}>
-                  <p className={cx("ellipsis")}>{account.email}</p>
-                </TooltipRoot>
-              </TableCellUI>
-              <TableCellUI>
-                <div className={cx("buttonContainer")}>
-                  <ChipButton onClick={() => {
-                    setFocusedAccount({ memberId: account.memberId, nickName: account.nickName, target: "reissue" });
-                  }}
-                  >
-                    비밀번호 재발급
-                  </ChipButton>
-                  <ChipButton onClick={() => {
-                    setFocusedAccount({ memberId: account.memberId, nickName: account.nickName, target: "delete" });
-                  }}
-                  >
-                    계정 탈퇴
-                  </ChipButton>
-                </div>
-              </TableCellUI>
-            </TableRowUI>
-          );
-        })}
-      </TableBodyUI>
-    </TableContainerUI>
+    <>
+      <TableContainerUI
+        stickyLastCol
+        tableWidth={1110}
+        paginationElement={paginationElement}
+      >
+        <TableHeaderUI>
+          <TableCellUI isHeader>닉네임</TableCellUI>
+          <TableCellUI isHeader>계정 ID</TableCellUI>
+          {/* // TODO: 어드민 타입? */}
+          <TableCellUI isHeader colWidth={330}>계정 이메일</TableCellUI>
+          <TableCellUI isHeader colWidth={232}>비고</TableCellUI>
+        </TableHeaderUI>
+        <TableBodyUI>
+          {accounts.map((account) => {
+            return (
+              <TableRowUI key={account.memberId}>
+                <TableCellUI>
+                  <TooltipRoot message={account.nickName}>
+                    <p className={cx("ellipsis")}>{account.nickName}</p>
+                  </TooltipRoot>
+                </TableCellUI>
+                <TableCellUI>
+                  <TooltipRoot message={account.loginId}>
+                    <p className={cx("ellipsis")}>{account.loginId}</p>
+                  </TooltipRoot>
+                </TableCellUI>
+                <TableCellUI>
+                  <TooltipRoot message={account.email}>
+                    <p className={cx("ellipsis")}>{account.email}</p>
+                  </TooltipRoot>
+                </TableCellUI>
+                <TableCellUI>
+                  <div className={cx("buttonContainer")}>
+                    <ChipButton onClick={() => {
+                      setFocusedAccount({ memberId: account.memberId, nickName: account.nickName, target: "reissue" });
+                    }}
+                    >
+                      비밀번호 재발급
+                    </ChipButton>
+                    <ChipButton onClick={() => {
+                      setFocusedAccount({ memberId: account.memberId, nickName: account.nickName, target: "delete" });
+                    }}
+                    >
+                      계정 탈퇴
+                    </ChipButton>
+                  </div>
+                </TableCellUI>
+              </TableRowUI>
+            );
+          })}
+        </TableBodyUI>
+      </TableContainerUI>
+      <ReissuedPasswordModal
+        newPassword={newPassword!}
+        open={isOpenReissuedPwModal}
+        onClose={() => { setIsOpenReissuedPwModal(false); }}
+      />
+    </>
   );
 };
 
