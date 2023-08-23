4b682c0dfe303a558b0f620ba6e8de1252fcfe74
hayoung-99 <nhy990130@gmail.com>
2023-08-23 13:44:15 +0900

    feat: 프로필 이미지, 대표곡 colWidth 적용 & formatMoney 적용

--- src/components/artist/ArtistsStatusTable/ArtistsStatusTable.tsx
+++ src/components/artist/ArtistsStatusTable/ArtistsStatusTable.tsx
@@ -5,6 +5,7 @@ import TableContainerUI from "@/components/common/Table/Composition/TableContain
 import TableHeaderUI from "@/components/common/Table/Composition/TableHeaderUI";
 import TableRowUI from "@/components/common/Table/Composition/TableRowUI";
 import { IArtistList } from "@/types/dto";
+import formatMoney from "@/utils/formatMoney";
 
 import ArtistProfileImage from "../ArtistProfileImage/ArtistProfileImage";
 
@@ -23,12 +24,13 @@ const ArtistsStatusTable = ({
       tableWidth={1200}
     >
       <TableHeaderUI>
-        <TableCellUI isHeader> </TableCellUI>
+        <TableCellUI isHeader colWidth={90}> </TableCellUI>
+        {/* TODO: 아티스트 한영문 통합 & 프로필 이미지 테이블 cell width 조정 */}
         <TableCellUI isHeader>아티스트명</TableCellUI>
         <TableCellUI isHeader>매출액</TableCellUI>
         <TableCellUI isHeader>회사 이익</TableCellUI>
         <TableCellUI isHeader>정산액</TableCellUI>
-        <TableCellUI isHeader>당월 대표곡</TableCellUI>
+        <TableCellUI isHeader colWidth={350}>당월 대표곡</TableCellUI>
         <TableCellUI isHeader>전월 대비 상승/하락율</TableCellUI>
       </TableHeaderUI>
       <TableBodyUI>
@@ -42,9 +44,9 @@ const ArtistsStatusTable = ({
                 />
               </TableCellUI>
               <TableCellUI>{artistInfo.artist.koArtistName}</TableCellUI>
-              <TableCellUI>{artistInfo.revenue}</TableCellUI>
-              <TableCellUI>{artistInfo.netIncome}</TableCellUI>
-              <TableCellUI>{artistInfo.settlementAmount}</TableCellUI>
+              <TableCellUI>{`${formatMoney(artistInfo.revenue, "table")}원`}</TableCellUI>
+              <TableCellUI>{`${formatMoney(artistInfo.netIncome, "table")}원`}</TableCellUI>
+              <TableCellUI>{`${formatMoney(artistInfo.settlementAmount, "table")}원`}</TableCellUI>
               <TableCellUI>{artistInfo.representativeTrack}</TableCellUI>
               <TableCellUI>
                 <Chip percentage={artistInfo.monthlyIncreaseRate} />
