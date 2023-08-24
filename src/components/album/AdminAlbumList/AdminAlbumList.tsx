8b13dff9cb725b7f924b6b50dbbb42f70876ca20
hayoung-99 <nhy990130@gmail.com>
2023-08-24 21:25:51 +0900

    feat: 어드민용 AlbumList 컴포넌트 제작

---
+++ src/components/album/AdminAlbumList/AdminAlbumList.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames/bind";
+
+import AlbumCard from "@/components/common/AlbumCard/AlbumCard";
+import { MOCK_ALBUMS } from "@/constants/mock";
+
+import styles from "./AdminAlbumList.module.scss";
+
+const cx = classNames.bind(styles);
+
+const AdminAlbumList = () => {
+  const albumList = MOCK_ALBUMS.contents;
+
+  return (
+    <div className={cx("container")}>
+      {albumList.map((album) => {
+        return (
+          <AlbumCard
+            key={album.albumId}
+            albumId={album.albumId}
+            albumCoverUrl={album.albumImage}
+            albumTitle={album.koAlbumName}
+            hasKebabButton
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default AdminAlbumList;
--- src/components/album/AlbumListSection/AlbumListSection.tsx
+++ src/components/album/AlbumListSection/AlbumListSection.tsx
@@ -4,6 +4,8 @@ import MainLayout from "@/components/common/Layouts/MainLayout";
 import SearchBar from "@/components/common/SearchBar/SearchBar";
 import useToast from "@/hooks/useToast";
 
+import AdminAlbumList from "../AdminAlbumList/AdminAlbumList";
+
 import styles from "./AlbumListSection.module.scss";
 
 const cx = classNames.bind(styles);
@@ -18,11 +20,11 @@ const AlbumListSection = () => {
   return (
     <MainLayout title="앨범 상세">
       <div className={cx("artboardLayout")}>
-        <div style={{ width: 850 }}>
+        <div className={cx("content")}>
           <div className={cx("searchBarSection")}>
             <SearchBar placeholder="앨범명을 검색해주세요." onClick={handleSearchAlbumTitle} />
           </div>
-
+          <AdminAlbumList />
         </div>
       </div>
     </MainLayout>
