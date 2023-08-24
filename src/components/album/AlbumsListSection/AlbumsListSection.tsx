6d2b033066521049925312e9c95dccec677609b9
hayoung-99 <nhy990130@gmail.com>
2023-08-24 21:06:19 +0900

    feat: 앨범명 검색바 추가

--- src/components/album/AlbumsListSection/AlbumsListSection.tsx
+++ src/components/album/AlbumsListSection/AlbumsListSection.tsx
@@ -1,16 +1,28 @@
 import classNames from "classnames/bind";
 
 import MainLayout from "@/components/common/Layouts/MainLayout";
+import SearchBar from "@/components/common/SearchBar/SearchBar";
+import useToast from "@/hooks/useToast";
 
 import styles from "./AlbumsListSection.module.scss";
 
 const cx = classNames.bind(styles);
 
 const AlbumListSection = () => {
+  const { showToast } = useToast();
+
+  const handleSearchAlbumTitle = () => {
+    showToast("앨범명 검색!");
+  };
+
   return (
     <MainLayout title="앨범 상세">
       <div className={cx("artboardLayout")}>
-        <div style={{ width: 850 }}>앨범 상세</div>
+        <div style={{ width: 850 }}>
+          <div className={cx("searchBarSection")}>
+            <SearchBar placeholder="앨범명을 검색해주세요." onClick={handleSearchAlbumTitle} />
+          </div>
+        </div>
       </div>
     </MainLayout>
   );
