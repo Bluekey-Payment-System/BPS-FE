faefbec5dc2244dad6e3e4a0fb9e3f8fdb7cc8c1
hayoung-99 <nhy990130@gmail.com>
2023-08-26 21:53:55 +0900

    fix: keyword=null인 경우 ""로 처리

--- src/services/api/requests/albums/albums.get.api.ts
+++ src/services/api/requests/albums/albums.get.api.ts
@@ -11,7 +11,7 @@ import { getRequest } from "../requests.api";
 
 /* 앨범 리스트 조회 */
 const getAlbums = async (page: number, size: number, keyword: string | null) => {
-  const response = getRequest<IGetAlbumsResponse>(`/albums?page=${page}&size=${size}&keyword=${keyword}`);
+  const response = getRequest<IGetAlbumsResponse>(`/albums?page=${page}&size=${size}&keyword=${keyword ?? ""}`);
   return response;
 };
 
