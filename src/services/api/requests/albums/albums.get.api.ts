d85d59ea3cb9d2a706b9f659b3a4982275ef7d18
drizzle96 <dlxkffldk37@naver.com>
2023-08-26 22:12:18 +0900

    style: api 함수명 통일

--- src/services/api/requests/albums/albums.get.api.ts
+++ src/services/api/requests/albums/albums.get.api.ts
@@ -10,46 +10,37 @@ import {
 import { getRequest } from "../requests.api";
 
 /* 앨범 리스트 조회 */
-const getAlbums = async (page: number, size: number, keyword: string | null) => {
+export const getAlbums = async (page: number, size: number, keyword: string | null) => {
   const response = getRequest<IGetAlbumsResponse>(`/albums?page=${page}&size=${size}&keyword=${keyword ?? ""}`);
   return response;
 };
 
 /* 앨범의 트랙 리스트 조회 */
-const getAlbumTracks = async (albumId: number) => {
+export const getAlbumTracks = async (albumId: number) => {
   const response = getRequest<IGetAlbumTracksResponse>(`/albums/${albumId}`);
   return response;
 };
 
 /* 앨범의 월별 정산액 */
-const getAlbumMonthlySettlement = async (albumId: number, startDate: string, endDate: string) => {
+export const getAlbumDashboardBar = async (albumId: number, startDate: string, endDate: string) => {
   const response = getRequest<IGetAlbumMonthlySettlementResponse>(`/albums/${albumId}/dashboard?startDate=${convertYearMonthToQuery(startDate)}&endDate=${convertYearMonthToQuery(endDate)}`);
   return response;
 };
 
 /* 앨범 대시보드에 들어갈 기본 정보 */
-const getAlbumDashboard = async (albumId: number, month: string) => {
+export const getAlbumDashboardCards = async (albumId: number, month: string) => {
   const response = getRequest<IGetAlbumDashboardResponse>(`/albums/${albumId}/dashboard/summary?monthly=${convertYearMonthToQuery(month)}`);
   return response;
 };
 
 /* 앨범의 당월 매출 Top N 트랙 리스트 정보 */
-const getAlbumRevenueTopTrack = async (albumId: number, month: string, rank: number) => {
+export const getAlbumDashboardDoughnut = async (albumId: number, month: string, rank: number) => {
   const response = getRequest<IGetAlbumRevenueTopTrackResponse>(`/albums/${albumId}/dashboard/topTrack?monthly=${convertYearMonthToQuery(month)}&rank=${rank}`);
   return response;
 };
 
 /* 앨범의 트랙별 정산 리스트 */
-const getAlbumTrackSettlementTrends = (albumId: number, startDate: string, endDate: string) => {
+export const getAlbumDashboardLine = (albumId: number, startDate: string, endDate: string) => {
   const response = getRequest(`/albums/${albumId}/dashboard/track?startDate=${convertYearMonthToQuery(startDate)}&endDate=${convertYearMonthToQuery(endDate)}`);
   return response;
-};
-
-export {
-  getAlbums,
-  getAlbumTracks,
-  getAlbumMonthlySettlement,
-  getAlbumDashboard,
-  getAlbumRevenueTopTrack,
-  getAlbumTrackSettlementTrends,
 };
