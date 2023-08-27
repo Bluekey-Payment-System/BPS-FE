ad65fec8c4eabb1eb657ff4d9aa28a5ea2cdd24d
SeyoungCho <wnl383@naver.com>
2023-08-27 18:42:33 +0900

    feat: AddTrackForm에 useAddTrack 뮤테이션 훅 도입

--- src/components/album/AddTrackModal/AddTrackForm.tsx
+++ src/components/album/AddTrackModal/AddTrackForm.tsx
@@ -1,3 +1,5 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
 import classNames from "classnames/bind";
 
 import Button from "@/components/common/CommonBtns/Button/Button";
@@ -6,22 +8,41 @@ import Checkbox from "@/components/common/Inputs/Checkbox/Checkbox";
 import TextField from "@/components/common/Inputs/TextField/TextField";
 import TextFieldWithUnit from "@/components/common/Inputs/TextFieldWithUnit/TextFieldWithUnit";
 import Spacing from "@/components/common/Layouts/Spacing";
+import useAddAlbumTrack from "@/services/queries/albums/useAddAlbumTrack";
+import { ITrackFieldValues } from "@/types/album.types";
+import { IAlbumInfo } from "@/types/dto";
 
 import styles from "./AddTrackForm.module.scss";
 
 const cx = classNames.bind(styles);
 
-const AddTrackForm = () => {
+const AddTrackForm = ({ albumInfo }: { albumInfo: IAlbumInfo }) => {
+  const { register, formState: { errors }, handleSubmit } = useForm<ITrackFieldValues>();
+  const { mutate, isLoading } = useAddAlbumTrack(albumInfo.albumId);
+  const onSubmit: SubmitHandler<ITrackFieldValues> = (data) => {
+    mutate(data);
+  };
   return (
-    <form className={cx("container")}>
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <form className={cx("container")} onSubmit={handleSubmit(onSubmit)}>
       <div className={cx("checkBoxContainer")}>
-        <Checkbox label="블루키 오리지널 트랙" />
+        <Checkbox label="블루키 오리지널 트랙" {...register("originalTrack")} />
       </div>
       <div className={cx("trackNameSection")}>
         <h2>수록곡 추가</h2>
         <div className={cx("inputArea")}>
-          <TextField label="트랙명 (한글)" placeholder="트랙명을 입력해주세요" errors={{}} />
-          <TextField label="트랙명 (영문)" placeholder="영문 트랙명을 입력해주세요" errors={{}} />
+          <TextField
+            label="트랙명 (한글)"
+            {...register("name")}
+            placeholder="트랙명을 입력해주세요"
+            errors={errors}
+          />
+          <TextField
+            label="트랙명 (영문)"
+            {...register("enName")}
+            placeholder="영문 트랙명을 입력해주세요"
+            errors={errors}
+          />
         </div>
       </div>
       <div className={cx("divider")} />
@@ -40,7 +61,9 @@ const AddTrackForm = () => {
       </div>
       <Spacing size={26} />
       <div className={cx("buttonSection")}>
-        <Button size="medium" theme="dark" type="submit">추가하기</Button>
+        <Button size="medium" theme="dark" type="submit">
+          {isLoading ? "추가하는 중..." : "추가하기"}
+        </Button>
         <Button size="medium" theme="bright" type="button">창 닫기</Button>
       </div>
     </form>
