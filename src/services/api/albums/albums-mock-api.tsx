34d4d10be2e6d305af807795e1653331db919b79
hayoung-99 <nhy990130@gmail.com>
2023-08-28 04:08:19 +0900

    feat: user type에 따른 분기조건 추가

--- src/services/api/albums/albums-mock-api.tsx
+++ src/services/api/albums/albums-mock-api.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { MOCK_ALBUMS } from "@/constants/mock";
+import { MEMBER_TYPE, MemberType } from "@/types/enums/user.enum";
 
 import { IGetAlbumsResponse } from "../types/albums";
 
@@ -21,8 +22,8 @@ const getAdminAlbums = (
 const getArtistAlbums = (
   page: number,
   size: number,
-  memberId: number,
   keyword: string | null,
+  memberId: number,
 ): Promise<IGetAlbumsResponse> => {
   // TODO: (GET) 아티스트 본인이 참여한 앨범 리스트 가져오기
   return new Promise((resolve) => {
@@ -34,4 +35,22 @@ const getArtistAlbums = (
   });
 };
 
-export { getAdminAlbums, getArtistAlbums };
+const getAlbums = async (
+  type: MemberType,
+  page: number,
+  size: number,
+  keyword: string | null,
+  memberId?: number,
+) => {
+  let response;
+
+  if (type === MEMBER_TYPE.ADMIN) {
+    response = await getAdminAlbums(page, size, keyword);
+  } else {
+    response = await getArtistAlbums(page, size, keyword, memberId as number);
+  }
+
+  return response;
+};
+
+export { getAlbums };
