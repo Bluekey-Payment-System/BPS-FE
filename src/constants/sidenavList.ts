e157262eae089b5658d35fc1ba80321dbc1b611e
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-28 05:03:02 +0900

    fix: 대시보드 리스트 수정

--- src/constants/sidenavList.ts
+++ src/constants/sidenavList.ts
@@ -3,7 +3,7 @@ import getLatestYearMonthString from "@/utils/getLatestYearMonthString";
 
 export const SIDE_NAV_ITEMS_SUPER_ADMIN = [
   { id: 1, content: "대시보드", path: `/admin/dashboard/${getLatestYearMonthString()}` },
-  { id: 2, content: "아티스트 상세", path: `/admin/artists/${getLatestYearMonthString()}` },
+  { id: 2, content: "아티스트 현황", path: `/admin/artists/${getLatestYearMonthString()}` },
   { id: 3, content: "아티스트 등록", path: "/admin/artists/new" },
   { id: 4, content: "타 계정 관리", path: "/admin/manage-accounts" },
   { id: 5, content: "앨범 상세", path: "/admin/albums" },
@@ -14,7 +14,7 @@ export const SIDE_NAV_ITEMS_SUPER_ADMIN = [
 
 export const SIDE_NAV_ITEMS_ADMIN = [
   { id: 1, content: "대시보드", path: `/admin/dashboard/${getLatestYearMonthString()}` },
-  { id: 2, content: "아티스트 상세", path: `/admin/artists/${getLatestYearMonthString()}` },
+  { id: 2, content: "아티스트 현황", path: `/admin/artists/${getLatestYearMonthString()}` },
   { id: 3, content: "아티스트 등록", path: "/admin/artists/new" },
   { id: 4, content: "아티스트 계정 관리", path: "/admin/manage-accounts" },
   { id: 5, content: "앨범 상세", path: "/admin/albums" },
@@ -27,6 +27,6 @@ const userId = store.getState().user.member?.memberId;
 
 export const SIDE_NAV_ITEMS_ARTIST = [
   { id: 1, content: "대시보드", path: `/artists/${userId}/dashboard/${getLatestYearMonthString()}` },
-  { id: 2, content: "앨범 상세", path: `/artists/${userId}/albums` },
+  { id: 2, content: "앨범 탐색", path: `/artists/${userId}/albums` },
   { id: 3, content: "내 프로필", path: `/artists/${userId}/my-profile` },
 ];
