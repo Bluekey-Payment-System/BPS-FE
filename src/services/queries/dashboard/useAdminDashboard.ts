fc1ef2e133b254e687813c77b894cc89107493a6
drizzle96 <dlxkffldk37@naver.com>
2023-08-27 22:41:10 +0900

    feat: 어드민, 아티스트 대시보드의 테이블에 페이지 prefetch 적용

--- src/services/queries/dashboard/useAdminDashboard.ts
+++ src/services/queries/dashboard/useAdminDashboard.ts
@@ -1,7 +1,11 @@
+/* eslint-disable no-void */
 /* eslint-disable max-len */
 
-import { useQueries } from "@tanstack/react-query";
+import { useEffect } from "react";
 
+import { useQueries, useQueryClient } from "@tanstack/react-query";
+
+import { ITEMS_PER_DASHBOARD_TABLE } from "@/constants/pagination";
 import { DASHBOARD_TYPE } from "@/types/enums/dashboard.enum";
 
 import { getDashboardCards } from "./queryFns/cards";
@@ -46,6 +50,53 @@ const useAdminDashboard = (
       },
     ],
   });
+
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const totalItems = queries[3].data?.totalItems;
+    const endPage = totalItems ? Math.ceil(totalItems / ITEMS_PER_DASHBOARD_TABLE) : 0;
+    // 1. 끝 페이지
+    if (endPage) {
+      void queryClient.prefetchQuery(
+        [DASHBOARD_TYPE.ADMIN, "dashboard", "table", null, {
+          month, page: endPage, sortBy, searchBy, keyword,
+        }],
+        () => { return getDashboardTable(DASHBOARD_TYPE.ADMIN, month, endPage, sortBy, searchBy, keyword); },
+      );
+    }
+
+    // 2. 1페이지
+    void queryClient.prefetchQuery(
+      [DASHBOARD_TYPE.ADMIN, "dashboard", "table", null, {
+        month, page: 1, sortBy, searchBy, keyword,
+      }],
+      () => { return getDashboardTable(DASHBOARD_TYPE.ADMIN, month, 1, sortBy, searchBy, keyword); },
+    );
+
+    const curPaginationStartPage = Math.floor((page - 1) / ITEMS_PER_DASHBOARD_TABLE) * ITEMS_PER_DASHBOARD_TABLE + 1;
+    const nextPaginationStartPage = curPaginationStartPage + ITEMS_PER_DASHBOARD_TABLE;
+    const prevPaginationstartPage = curPaginationStartPage - ITEMS_PER_DASHBOARD_TABLE;
+
+    // 3. 현재 페이지네이션의 시작 페이지 ~ 다음 페이지네이션의 시작 페이지
+    for (let i = curPaginationStartPage; i <= Math.min(endPage, nextPaginationStartPage); i += 1) {
+      void queryClient.prefetchQuery(
+        [DASHBOARD_TYPE.ADMIN, "dashboard", "table", null, {
+          month, page: i, sortBy, searchBy, keyword,
+        }],
+        () => { return getDashboardTable(DASHBOARD_TYPE.ADMIN, month, i, sortBy, searchBy, keyword); },
+      );
+    }
+
+    // 4. 이전 페이지네이션의 시작 페이지
+    if (prevPaginationstartPage >= 1) {
+      void queryClient.prefetchQuery(
+        [DASHBOARD_TYPE.ADMIN, "dashboard", "table", null, {
+          month, page: prevPaginationstartPage, sortBy, searchBy, keyword,
+        }],
+        () => { return getDashboardTable(DASHBOARD_TYPE.ADMIN, month, prevPaginationstartPage, sortBy, searchBy, keyword); },
+      );
+    }
+  }, [keyword, month, page, queries, queryClient, searchBy, sortBy]);
 
   return queries;
 };
