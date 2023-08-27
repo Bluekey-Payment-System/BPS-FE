95df4cde866005481431f94b576558eabf987faa
hayoung-99 <nhy990130@gmail.com>
2023-08-28 04:14:37 +0900

    feat: useAlbums 쿼리 훅 제작

---
+++ src/services/queries/albums/useAlbumList.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { ITEMS_PER_ALBUM_LIST } from "@/constants/pagination";
+import { getAlbums } from "@/services/api/albums/albums-mock-api";
+import { MemberType } from "@/types/enums/user.enum";
+
+const useAlbums = (type: MemberType, page: number, keyword: string, memberId?: number) => {
+  const query = useQuery(
+    ["album-list", `memberId=${memberId}`],
+    () => { return getAlbums(type, page, ITEMS_PER_ALBUM_LIST, keyword); },
+    {
+      staleTime: Infinity,
+    },
+  );
+
+  return query;
+};
+
+export { useAlbums };
