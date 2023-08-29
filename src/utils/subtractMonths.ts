afa4883b0bdfd025ae742b7176d8ce6a3d728b13
임병욱 <imbyeong-wook@imbyeong-ug-ui-MacBookPro.local>
2023-08-29 20:39:26 +0900

    docs: jsdoc 추가

--- src/utils/subtractMonths.ts
+++ src/utils/subtractMonths.ts
@@ -1,4 +1,11 @@
 /* eslint-disable radix */
+
+/**
+ * @author 임병욱
+ * @param {string} yyyymm 원하는 날짜 yyyymm 형식
+ * @param {number} months 지정된 날짜에서 number에 해당하는 월만큼 빼기
+ * @return {string} ex)202306, 3이 입력되면 202304 출력
+*/
 const subtractMonths = (yyyymm: string, months: number): string[] => {
   const year = parseInt(yyyymm.slice(0, 4));
   const month = parseInt(yyyymm.slice(4)) - 1; // 월은 0부터 11로 표현됨
