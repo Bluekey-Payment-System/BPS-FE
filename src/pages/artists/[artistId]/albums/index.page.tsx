3cf8d96df204b8a183bd654bd4c39ca1872cddbe
hayoung-99 <nhy990130@gmail.com>
2023-08-30 23:04:28 +0900

    feat: 아티스트가 보는 앨범 탐색 로딩 처리

--- src/pages/artists/[artistId]/albums/index.page.tsx
+++ src/pages/artists/[artistId]/albums/index.page.tsx
@@ -1,9 +1,13 @@
+import classNames from "classnames/bind";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 import AlbumListSection from "@/components/album/AlbumListSection/AlbumListSection";
+import Orbit from "@/components/common/Loading/Orbit";
 import useAlbums from "@/services/queries/albums/useAlbumList";
 import { MEMBER_TYPE } from "@/types/enums/user.enum";
 import convertPageParamToNum from "@/utils/convertPageParamToNum";
+
+import styles from "./index.module.scss";
 
 interface ServerSidePageProps {
   memberId: number,
@@ -11,21 +15,27 @@ interface ServerSidePageProps {
   keyword: string | null,
 }
 
+const cx = classNames.bind(styles);
+
 const ArtistAlbumListPage = (
   query: InferGetServerSidePropsType<GetServerSideProps<ServerSidePageProps>>,
 ) => {
   const { memberId, page, keyword }: ServerSidePageProps = query;
   const albumsQuery = useAlbums(MEMBER_TYPE.USER, page, keyword, memberId);
   const {
-    data: albumsData, isLoading, isError, isFetching,
+    data: albumsData, isLoading,
   } = albumsQuery;
 
-  if (isLoading || isFetching) return <div>로딩 중...</div>;
-  if (isError) return <div>에러 발생</div>;
-  if (!albumsData) return <div>데이터 없음</div>;
+  if (isLoading) {
+    return (
+      <div className={cx("loading")}>
+        <Orbit dark />
+      </div>
+    );
+  }
 
   return (
-    <AlbumListSection albumList={albumsData.contents} userType={MEMBER_TYPE.USER} page={page} keyword={keyword ?? ""} totalAlbumItems={albumsData.totalItems} />
+    <AlbumListSection albumList={albumsData!.contents} userType={MEMBER_TYPE.USER} page={page} keyword={keyword ?? ""} totalAlbumItems={albumsData!.totalItems} />
   );
 };
 
