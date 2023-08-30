42ec17a84d6d0a483535b32de8cb0643102f7c36
drizzle96 <dlxkffldk37@naver.com>
2023-08-30 22:31:08 +0900

    feat: 계정관리 페이지에 api 연동 및 로딩 처리

--- src/services/queries/manage-accounts/useAccounts.ts
+++ src/services/queries/manage-accounts/useAccounts.ts
@@ -1,30 +1,105 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useQuery } from "@tanstack/react-query";
+/* eslint-disable max-len */
+/* eslint-disable no-void */
+import { useEffect } from "react";
 
-import { MOCK_ACCOUNTS } from "@/constants/mock";
-import { IGetAccountsResponse } from "@/services/api/types/member";
-import { MEMBER_TYPE } from "@/types/enums/user.enum";
+import { useQueries, useQueryClient } from "@tanstack/react-query";
 
-export const getAccounts = (
-  artistPage: number,
-  adminPage: number,
-): Promise<IGetAccountsResponse> => {
-  return new Promise((resolve) => {
-    setTimeout(() => { return resolve(MOCK_ACCOUNTS); }, 2000);
+import { ITEMS_PER_ACCOUNTS_TABLE, PAGES_PER_PAGINATION } from "@/constants/pagination";
+import { getAdminAccounts, getArtistAccounts } from "@/services/api/requests/admin/admin.get.api";
+import { MEMBER_ROLE, MEMBER_TYPE, MemberRole } from "@/types/enums/user.enum";
+
+const useAccounts = (artistPage: number, adminPage: number, memberRole: MemberRole) => {
+  const queries = useQueries({
+    queries: [
+      {
+        queryKey: [MEMBER_TYPE.ADMIN, "manage-accounts", { artistPage }],
+        queryFn: () => {
+          return getArtistAccounts(artistPage, ITEMS_PER_ACCOUNTS_TABLE);
+        },
+      },
+      {
+        queryKey: [MEMBER_TYPE.ADMIN, "manage-accounts", { adminPage }],
+        queryFn: () => {
+          return getAdminAccounts(adminPage, ITEMS_PER_ACCOUNTS_TABLE);
+        },
+        enabled: (memberRole === MEMBER_ROLE.SUPER_ADMIN),
+      },
+    ],
   });
-};
 
-const useAccounts = (artistPage: number, adminPage: number) => {
-  const { data: accounts, isLoading: isAccountsLoading, isError: isAccountsError } = useQuery(
-    // TODO: 두 개의 독립적인 페이지 파람 어떻게 쿼리키에 대입?
-    // -> BE에 account api 분리 요청 중..
-    [MEMBER_TYPE.ADMIN, "manage-accounts"],
-    () => { return getAccounts(artistPage, adminPage); },
-  );
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const totalItems = queries[0].data?.totalItems;
+    const endPage = totalItems ? Math.ceil(totalItems / PAGES_PER_PAGINATION) : 0;
+    // 1. 끝 페이지
+    if (endPage) {
+      void queryClient.prefetchQuery(
+        [MEMBER_TYPE.ADMIN, "manage-accounts", { artistPage: endPage }],
+        () => { return getArtistAccounts(endPage, ITEMS_PER_ACCOUNTS_TABLE); },
+      );
+    }
+    // 2. 1페이지
+    void queryClient.prefetchQuery(
+      [MEMBER_TYPE.ADMIN, "manage-accounts", { artistPage: 1 }],
+      () => { return getArtistAccounts(1, ITEMS_PER_ACCOUNTS_TABLE); },
+    );
+    const curPaginationStartPage = Math.floor((artistPage - 1) / PAGES_PER_PAGINATION) * PAGES_PER_PAGINATION + 1;
+    const nextPaginationStartPage = curPaginationStartPage + PAGES_PER_PAGINATION;
+    const prevPaginationstartPage = curPaginationStartPage - PAGES_PER_PAGINATION;
+    // 3. 현재 페이지네이션의 시작 페이지 ~ 다음 페이지네이션의 시작 페이지
+    for (let i = curPaginationStartPage; i <= Math.min(endPage, nextPaginationStartPage); i += 1) {
+      void queryClient.prefetchQuery(
+        [MEMBER_TYPE.ADMIN, "manage-accounts", { artistPage: i }],
+        () => { return getArtistAccounts(i, ITEMS_PER_ACCOUNTS_TABLE); },
+      );
+    }
+    // 4. 이전 페이지네이션의 시작 페이지
+    if (prevPaginationstartPage >= 1) {
+      void queryClient.prefetchQuery(
+        [MEMBER_TYPE.ADMIN, "manage-accounts", { artistPage: prevPaginationstartPage }],
+        () => { return getArtistAccounts(prevPaginationstartPage, ITEMS_PER_ACCOUNTS_TABLE); },
+      );
+    }
+  }, [artistPage, queries, queryClient]);
 
-  return {
-    accounts, isAccountsLoading, isAccountsError,
-  };
+  useEffect(() => {
+    // 슈퍼 어드민 타입이 아닌 경우 prefetch X
+    if (memberRole === MEMBER_ROLE.SUPER_ADMIN) {
+      const totalItems = queries[1].data?.totalItems;
+      const endPage = totalItems ? Math.ceil(totalItems / ITEMS_PER_ACCOUNTS_TABLE) : 0;
+      // 1. 끝 페이지
+      if (endPage) {
+        void queryClient.prefetchQuery(
+          [MEMBER_TYPE.ADMIN, "manage-accounts", { adminPage: endPage }],
+          () => { return getAdminAccounts(endPage, ITEMS_PER_ACCOUNTS_TABLE); },
+        );
+      }
+      // 2. 1페이지
+      void queryClient.prefetchQuery(
+        [MEMBER_TYPE.ADMIN, "manage-accounts", { adminPage: 1 }],
+        () => { return getAdminAccounts(1, ITEMS_PER_ACCOUNTS_TABLE); },
+      );
+      const curPaginationStartPage = Math.floor((adminPage - 1) / ITEMS_PER_ACCOUNTS_TABLE) * ITEMS_PER_ACCOUNTS_TABLE + 1;
+      const nextPaginationStartPage = curPaginationStartPage + ITEMS_PER_ACCOUNTS_TABLE;
+      const prevPaginationstartPage = curPaginationStartPage - ITEMS_PER_ACCOUNTS_TABLE;
+      // 3. 현재 페이지네이션의 시작 페이지 ~ 다음 페이지네이션의 시작 페이지
+      for (let i = curPaginationStartPage; i <= Math.min(endPage, nextPaginationStartPage); i += 1) {
+        void queryClient.prefetchQuery(
+          [MEMBER_TYPE.ADMIN, "manage-accounts", { adminPage: i }],
+          () => { return getAdminAccounts(i, ITEMS_PER_ACCOUNTS_TABLE); },
+        );
+      }
+      // 4. 이전 페이지네이션의 시작 페이지
+      if (prevPaginationstartPage >= 1) {
+        void queryClient.prefetchQuery(
+          [MEMBER_TYPE.ADMIN, "manage-accounts", { adminPage: prevPaginationstartPage }],
+          () => { return getAdminAccounts(prevPaginationstartPage, ITEMS_PER_ACCOUNTS_TABLE); },
+        );
+      }
+    }
+  }, [adminPage, memberRole, queries, queryClient]);
+
+  return queries;
 };
 
 export default useAccounts;
