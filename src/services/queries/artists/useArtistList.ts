6d4ce27c04cf65ed31bfb1f93010742ac28793a6
SeyoungCho <wnl383@naver.com>
2023-08-30 21:15:09 +0900

    feat: useArtistList훅에 드롭다운에서 쓰일 artistList를 포매팅하여 내보내는 기능 추가

--- src/services/queries/artists/useArtistList.ts
+++ src/services/queries/artists/useArtistList.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { useQuery } from "@tanstack/react-query";
 
 import { getDropdownArtists } from "@/services/api/requests/artist/artist.get.api";
@@ -5,12 +7,24 @@ import { IGetArtistsSimpleResponse } from "@/services/api/types/artist";
 import { MEMBER_ROLE } from "@/types/enums/user.enum";
 
 const useArtistList = () => {
-  const query = useQuery<IGetArtistsSimpleResponse>([MEMBER_ROLE.ARTIST, "names"], getDropdownArtists, {
+  const { data } = useQuery<IGetArtistsSimpleResponse>([MEMBER_ROLE.ARTIST, "names"], getDropdownArtists, {
     onError: (err) => {
       console.error(err);
     },
   });
-  return query;
+  const [artistList, setArtistList] = useState<{ id: number, name: string, commissionRate: number }[]>([{ id: -1, name: "", commissionRate: 0 }]);
+  useEffect(() => {
+    if (data) {
+      setArtistList(data.artists.map((artist) => {
+        return {
+          id: artist.memberId,
+          name: artist.name,
+          commissionRate: artist.commissionRate || 0,
+        };
+      }));
+    }
+  }, [data]);
+  return artistList;
 };
 
 export default useArtistList;
