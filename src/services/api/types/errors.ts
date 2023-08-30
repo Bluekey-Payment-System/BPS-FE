a6033876abd309c8e4b22b9da011f96c5872dea3
hayoung-99 <nhy990130@gmail.com>
2023-08-30 16:10:07 +0900

    feat: IPostTransactionUploadErrorResponse 에러 타입 제작

--- src/services/api/types/errors.ts
+++ src/services/api/types/errors.ts
@@ -1,3 +1,5 @@
+import { ITransactionUploadAlert } from "@/types/dto";
+
 interface FieldError {
   field: string;
   message: string;
@@ -7,4 +9,13 @@ export interface ICommonErrorResponse {
   message: string;
   code: string;
   fieldErrors: FieldError[] | null;
+}
+
+/* Transaction POST ERROR */
+/* 셀 값에서 문제가 있는 경우 */
+export interface IPostTransactionUploadErrorResponse {
+  code: string;
+  message: string;
+  totalErrorNums: number;
+  errors: ITransactionUploadAlert[];
 }
