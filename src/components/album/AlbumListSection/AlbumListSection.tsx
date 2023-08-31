e868e8bcd8a3e601378d7d77066139f2d1ba8f5a
drizzle96 <dlxkffldk37@naver.com>
2023-08-31 14:14:48 +0900

    feat: 앨범 탐색에서 데이터 없을 때 UI 구현

--- src/components/album/AlbumListSection/AlbumListSection.tsx
+++ src/components/album/AlbumListSection/AlbumListSection.tsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 import classNames from "classnames/bind";
 import { useRouter } from "next/router";
 
+import EmptyData from "@/components/common/EmptyData/EmptyData";
 import MainLayout from "@/components/common/Layouts/MainLayout";
 import Pagination from "@/components/common/Pagination/Pagination";
 import SearchBar from "@/components/common/SearchBar/SearchBar";
@@ -53,17 +54,28 @@ const AlbumListSection = ({
               <SearchBar placeholder="앨범명을 검색해주세요." onClick={handleSearchAlbumTitle} ref={searchKeywordRef} value={searchKeyword} />
             </form>
           </div>
-          <AlbumList
-            albumList={albumList}
-            userType={userType}
-            paginationElement={(
-              <Pagination
-                activePage={page}
-                totalItems={totalAlbumItems}
-                itemsPerPage={ITEMS_PER_ALBUM_LIST}
+          {totalAlbumItems === 0
+            ? (
+              <div className={cx("empty")}>
+                <EmptyData
+                  type={router.query?.keyword ? "no-search-result" : "no-data"}
+                  text={router.query?.keyword ? undefined : "데이터가 없습니다."}
+                />
+              </div>
+            )
+            : (
+              <AlbumList
+                albumList={albumList}
+                userType={userType}
+                paginationElement={(
+                  <Pagination
+                    activePage={page}
+                    totalItems={totalAlbumItems}
+                    itemsPerPage={ITEMS_PER_ALBUM_LIST}
+                  />
+                )}
               />
             )}
-          />
         </div>
       </div>
     </MainLayout>
