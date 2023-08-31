eaadc77df8ec5658285773c576f86365534f57df
drizzle96 <dlxkffldk37@naver.com>
2023-08-31 14:24:04 +0900

    fix: 앨범 등록 시 앨범 탐색 쿼리 무효화

--- src/services/queries/albums/useCreateAlbum.ts
+++ src/services/queries/albums/useCreateAlbum.ts
@@ -1,19 +1,22 @@
-import { useMutation } from "@tanstack/react-query";
+/* eslint-disable no-void */
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import useToast from "@/hooks/useToast";
 import { postAlbum } from "@/services/api/requests/albums/albums.post.api";
 import { IPostAlbumData, IPostAlbumResponse } from "@/services/api/types/albums";
+import { MEMBER_TYPE } from "@/types/enums/user.enum";
 
 const useCreateAlbum = () => {
   const { showToast } = useToast();
-  const mutation = useMutation<
-  IPostAlbumResponse,
-  unknown,
-  IPostAlbumData,
-  unknown
-  >(postAlbum, {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<IPostAlbumResponse, unknown, IPostAlbumData, unknown>(postAlbum, {
     onSuccess: (data) => {
       showToast(`"${data.name}" 앨범 생성이 완료되었습니다.`);
+      void queryClient.invalidateQueries({
+        queryKey: [MEMBER_TYPE.ADMIN, "albums"],
+        refetchType: "all",
+      });
     },
   });
 
