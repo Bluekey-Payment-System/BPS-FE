6fa9b3f1eb72b33876ae222a4ba56db646e0f76a
SeyoungCho <wnl383@naver.com>
2023-09-02 16:46:06 +0900

    fix: 비밀번호 재설정 요청 함수 patch요청 함수로 수정

--- src/services/api/requests/auth/auth.patch.api.ts
+++ src/services/api/requests/auth/auth.patch.api.ts
@@ -1,12 +1,12 @@
 import { IPatchChangePasswordRequest } from "../../types/auth";
-import { postRequest } from "../requests.api";
+import { patchRequest } from "../requests.api";
 
 /* 비밀번호 변경 */
 export const changePassword = async ({
   password,
 }: IPatchChangePasswordRequest) => {
   // 200번 response data 확인 요망
-  const response = await postRequest<string, IPatchChangePasswordRequest>("/auth/member/password", {
+  const response = await patchRequest<string, IPatchChangePasswordRequest>("/auth/member/password", {
     password,
   });
   return response;
