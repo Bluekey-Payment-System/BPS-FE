abeda3863e41122c9e8a416a90258277258d186a
hayoung-99 <nhy990130@gmail.com>
2023-09-02 17:42:47 +0900

    docs: 유틸함수 jsdoc 추가

--- src/components/upload-revenue/AlertDataTable/AlertDataTable.utils.ts
+++ src/components/upload-revenue/AlertDataTable/AlertDataTable.utils.ts
@@ -6,6 +6,11 @@ interface IExcelDataColumn {
   value: string
 }
 
+/**
+ * @author [hayoung-99](https://github.com/hayoung-99)
+ * @param data 정산 내역 업로드 시 발생할 수 있는 warnings / errors 데이터
+ * @returns 엑셀 파일에 들어가는 데이터 형태로 포맷
+ */
 export const formatToExcelData = (data: ITransactionUploadAlert[]): IExcelDataColumn[] => {
   const excelData = [];
 
