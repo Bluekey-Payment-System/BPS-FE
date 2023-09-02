92db7ef1ad699631742e1ee93dccbef8795ed788
hayoung-99 <nhy990130@gmail.com>
2023-09-02 17:29:37 +0900

    feat: 미등록 데이터 정보에 대한 엑셀 파일 헤더를 상수로 관리

---
+++ src/constants/alertDataHeaders.ts
@@ -0,0 +1,5 @@
+export const ALERT_DATA_HEADERS = [
+  { label: "Column Index", key: "columnIndex" }, // 행
+  { label: "Category", key: "category" }, // 분류
+  { label: "value", key: "value" }, // 값
+];
