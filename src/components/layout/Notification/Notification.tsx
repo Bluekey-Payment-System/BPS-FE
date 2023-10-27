a3f8faf3187a4a33124bb09fbbf283e843ff548b
wookki <bwlimtony@gmail.com>
2023-10-27 17:52:50 +0900

    chore: 단어수정

--- src/components/layout/Notification/Notification.tsx
+++ src/components/layout/Notification/Notification.tsx
@@ -11,7 +11,7 @@ const cx = classNames.bind(styles);
 // TODO) 권한 요청 목록 데이터 GET & 필터링된 데이터를 prop으로 넘겨주기
 
 const Notification = ({ onClickNotification }: { onClickNotification: () => void }) => {
-  const { data: authoritiyList } = useRequestAuthoritiesList();
+  const { data: authorityList } = useRequestAuthoritiesList();
   return (
     <Popover
       centerX
@@ -19,7 +19,7 @@ const Notification = ({ onClickNotification }: { onClickNotification: () => void
       onClose={() => { onClickNotification(); }}
     >
       <div className={cx("container")}>
-        {authoritiyList && <RequestAuthorityList data={authoritiyList.contents} />}
+        {authorityList && <RequestAuthorityList data={authorityList.contents} />}
       </div>
     </Popover>
   );
