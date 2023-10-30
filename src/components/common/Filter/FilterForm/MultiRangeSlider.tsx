84b441016c6b48639d6536704427f01c69fba6fd
drizzle96 <dlxkffldk37@naver.com>
2023-10-30 22:14:21 +0900

    feat: 초기값을 받을 수 있도록 구현, useEffect 로직 수정

--- src/components/common/Filter/FilterForm/MultiRangeSlider.tsx
+++ src/components/common/Filter/FilterForm/MultiRangeSlider.tsx
@@ -12,21 +12,25 @@ const cx = classNames.bind(styles);
 interface MultiRangeSliderProps {
   min: number;
   max: number;
+  initialMinValue?: number;
+  initialMaxValue?: number;
+  onChangeValue: (min: number, max: number) => void;
 }
 
-const MultiRangeSlider = ({ min, max }: MultiRangeSliderProps) => {
-  const [minVal, setMinVal] = useState(min);
-  const [maxVal, setMaxVal] = useState(max);
+const MultiRangeSlider = ({
+  min, max, initialMinValue, initialMaxValue, onChangeValue,
+}: MultiRangeSliderProps) => {
+  const [minVal, setMinVal] = useState(initialMinValue || min);
+  const [maxVal, setMaxVal] = useState(initialMaxValue || max);
   const minValRef = useRef(min);
   const maxValRef = useRef(max);
   const range = useRef<HTMLDivElement>(null);
   const leftValRef = useRef<HTMLDivElement>(null);
   const rightValRef = useRef<HTMLDivElement>(null);
 
-  // 왼쪽 thumb 움직일 때 range 너비 조정
   useEffect(() => {
     const minPercent = getPercent(minVal, min, max);
-    const maxPercent = getPercent(maxValRef.current, min, max);
+    const maxPercent = getPercent(maxVal, min, max);
     const distance = maxPercent - minPercent;
 
     if (range.current) {
@@ -39,25 +43,14 @@ const MultiRangeSlider = ({ min, max }: MultiRangeSliderProps) => {
     }
 
     if (rightValRef.current) {
-      rightValRef.current.style.marginTop = distance < 10 ? "-20px" : "20px";
-    }
-  }, [minVal, min, max]);
-
-  // 오른쪽 thumb 움직일 때 range 너비 조정
-  useEffect(() => {
-    const minPercent = getPercent(minValRef.current, min, max);
-    const maxPercent = getPercent(maxVal, min, max);
-    const distance = maxPercent - minPercent;
-
-    if (range.current) {
-      range.current.style.width = `${maxPercent - minPercent}%`;
-    }
-
-    if (rightValRef.current) {
       rightValRef.current.style.left = `${maxPercent}%`;
       rightValRef.current.style.marginTop = distance < 10 ? "-20px" : "20px";
     }
-  }, [maxVal, min, max]);
+  }, [minVal, maxVal, min, max]);
+
+  useEffect(() => {
+    onChangeValue(minVal, maxVal);
+  }, [minVal, maxVal, onChangeValue]);
 
   return (
     <div className={cx("container")}>
