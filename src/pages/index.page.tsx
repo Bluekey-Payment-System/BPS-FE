a1ca8e49f1c30655291a4f5e91e8653fdfb19261
SeyoungCho <wnl383@naver.com>
2023-12-03 17:31:34 +0900

    feat: 홈페이지 접속시 최근 대시보드로 리다이렉트하는 기능 추가

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,25 @@
+/* eslint-disable no-void */
+import { useEffect } from "react";
+
+import { useRouter } from "next/router";
+
+import LoadingSection from "@/components/common/Loading/LoadingSection";
+import { useAppSelector } from "@/redux/hooks";
+import { MEMBER_TYPE } from "@/types/enums/user.enum";
+import getLatestYearMonthString from "@/utils/getLatestYearMonthString";
+
 const Home = () => {
+  const userInfo = useAppSelector((state) => { return state.user.member; });
+  const router = useRouter();
+  useEffect(() => {
+    if (userInfo.type === MEMBER_TYPE.USER) {
+      void router.push(`/artists/${userInfo.memberId}/dashboard/${getLatestYearMonthString()}`);
+    }
+    void router.push(`/admin/dashboard/${getLatestYearMonthString()}`);
+  }, [router, userInfo.memberId, userInfo.type]);
+
   return (
-    <div>Home</div>
+    <LoadingSection />
   );
 };
 
